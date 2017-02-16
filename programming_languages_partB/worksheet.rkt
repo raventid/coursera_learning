2a4b65416968170b0d2b960e5e27717aed5fb6bc
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-02-16 21:45:12 +0300

    first summator function

--- programming_languages_partB/worksheet.rkt
+++ programming_languages_partB/worksheet.rkt
@@ -5,3 +5,9 @@
 (define xs (list 4 5 6))
 (define ys (list (list 1 2) 3 4))
 
+(define (sum1 xs)
+  (if (null? xs)
+      0
+      (if (number? (car xs))
+          (+ (car xs) (sum1 (cdr xs)))
+          (+ (sum1 (car xs)) (sum1 (cdr xs))))))
