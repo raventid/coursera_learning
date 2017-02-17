42b03e0e91df2f97dbcd16057d6d62f73ea87a23
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-02-17 22:05:12 +0300

    universal summator function

--- programming_languages_partB/worksheet.rkt
+++ programming_languages_partB/worksheet.rkt
@@ -11,3 +11,12 @@
       (if (number? (car xs))
           (+ (car xs) (sum1 (cdr xs)))
           (+ (sum1 (car xs)) (sum1 (cdr xs))))))
+
+(define (sum2 xs)
+  (if (null? xs)
+      0
+      (if (number? (car xs))
+          (+ (car xs) (sum2 (cdr xs)))
+          (if (list? (car xs))
+              (+ (sum2 (car xs)) (sum2 (cdr xs)))
+              (sum2 (cdr xs))))))
