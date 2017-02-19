ed0d6e582088d47836ec404444161fde3ed4de0c
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-02-19 23:11:35 +0300

    sum4 function list filtration and avoid types mismatch

--- programming_languages_partB/worksheet.rkt
+++ programming_languages_partB/worksheet.rkt
@@ -25,3 +25,9 @@
   (cond [(null? xs) 0]
         [(number? (car xs)) (+ (car xs) (sum3 (cdr xs)))]
         [#t (+ (sum3 (car xs)) (sum3 (cdr xs)))]))
+
+(define (sum4 xs)
+  (cond [(null? xs) 0]
+        [(number? (car xs)) (+ (car xs) (sum4 (cdr xs)))]
+        [(list? (car xs)) (+ (sum4 (car xs)) (sum4 (cdr xs)))]
+        [#t (sum4 (cdr xs))]))
