307c9e06b322db9d2e65340cb0e367279a221cd2
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-02-24 13:47:06 +0300

    last version of macro.rkt

--- programming_languages_partB/week1/macro.rkt
+++ programming_languages_partB/week1/macro.rkt
@@ -28,3 +28,14 @@
            (begin (set-mcar! x #t)
                   (set-mcdr! x ((mcdr x)))
                   (mcdr x))))]))
+
+(define-syntax for
+  (syntax-rules (to do)
+    [(for lo to hi do body)
+     (let ([l lo]
+           [h hi])
+       (letrec ([loop (lambda (it)
+                        (if (> it h)
+                            #t
+                            (begin body (loop (+ it 1)))))])
+         (loop l)))]))
