b03abb1a801bd04930d573fe5fc9fb7527315a73
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-02-28 21:27:21 +0300

    add few gates from nand week1

--- programming_languages_partB/week2/interpreter.rkt
+++ programming_languages_partB/week2/interpreter.rkt
@@ -2,6 +2,12 @@
 ; This file is not going to be a valid racket code
 ; it's more like kitchen sink for homework
 ; ATTENTION!!! ATTENTION!!! ATTENTION!!!
-(define (eval-under-env e env)
-  (cond ...
-   ))
+;(define (eval-under-env e env)
+;  (cond ...
+;   ))
+
+(struct hello (e) #:transparent)
+
+; How to implement closure?
+; Pretty straightforward, save env inside of a function
+(struct closure (env fun) #:transparent)
