9f63ff3788729d26399dc07e31ee547f881d8ff1
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-03-10 11:43:43 +0300

    queue for Rust experiments

---
+++ rust/queue.rs
@@ -0,0 +1,36 @@
+struct Queue {
+  older: Vec<char>,
+  younger: Vec<char>
+}
+
+impl Queue {
+  fn push(self: &mut Queue, c: char) {
+    self.younger.push(c);
+  }
+  
+  fn pop(self: &mut Queue) -> Option<char> {
+    if self.older.is_empty() {
+      if self.younger.is_empty() {
+        return None;
+      }
+      use std::mem::swap;
+      swap(&mut self.older, &mut self.younger);
+      self.older.reverse();
+    }
+
+    self.older.pop()
+  }
+}
+
+let mut q = Queue { older: Vec::new(), younger: Vec::new() };
+q.push('0');
+q.push('1');
+assert_eq!(q.pop(), Some('0'));
+
+q.push('w');
+assert_eq!(q.pop(), Some('1'));
+assert_eq!(q.pop(), Some('w'));
+assert_eq!(q.pop(), None);
+
+
+
