bdf3b081ab1de83861d5c7c78c042d74fdaad033
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-03-20 22:32:41 +0300

    enums conversion ( c-style )

--- rust/enums.rs
+++ rust/enums.rs
@@ -18,3 +18,12 @@ assert_eq!(size_of::<Pet>(), 1);
 assert_eq!(size_of::<HttpStatus>(), 2);
 
 assert_eq!(HttpStatus::Ok as i32, 200);
+
+fn to_http_status(i: u32) -> Option<HttpStatus> {
+  match i {
+    200 => Some(HttpStatus::Ok),
+    304 => Some(HttpStatus::NotModified),
+    404 => Some(HttpStatus::NotFound),
+    _ => None
+  }
+} 
