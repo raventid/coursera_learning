dcf6239e10d705f61b00561ee79447926712c5cb
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-03-23 21:01:52 +0300

    example of recursive structure in Rust, looks like idiomatic SML

---
+++ rust/json_ex.rs
@@ -0,0 +1,12 @@
+// Recursive structure/implementation of Json
+
+use std::collections::HashMap;
+
+enum Json {
+  Null,
+  Boolean(bool),
+  Number(f64),
+  String(String),
+  Array(Vec<Json>),
+  Object(Box<HashMap<String, Json>>)
+}
