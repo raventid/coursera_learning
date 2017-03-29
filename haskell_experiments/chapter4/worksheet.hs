058adca61e47a0cecaf3dbb8248a585ca042b3ad
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-03-29 20:12:43 +0300

    refreshing chapter 4

--- haskell_experiments/chapter4/worksheet.hs
+++ haskell_experiments/chapter4/worksheet.hs
@@ -3,21 +3,21 @@ module Worksheet where
 -- Data construction
 data My = False | True
 
--- Here we are using deriving which allows us to take something from Show 
+-- Here we are using deriving which allows us to take something from Show
 data Mood = Blah | Woot deriving Show
 
-changeMood :: Mood -> Mood 
+changeMood :: Mood -> Mood
 changeMood Blah = Woot
 changeMood    _ = Blah
 
 isPalindrom :: (Eq a) => [a] -> Bool
-isPalindrom x = x == reverse x 
+isPalindrom x = x == reverse x
 
 myAbs :: Integer -> Integer
 myAbs x = if x >= 0 then x else (-x)
 
 f :: (a, b) -> (c, d) -> ((b, d), (a, c))
-f (a,b)(c,d) = ((b,d), (a,c)) 
+f (a,b)(c,d) = ((b,d), (a,c))
 
 x = (+)
 myF xs = w `x` 1
@@ -27,4 +27,4 @@ myId = \x -> x
 
 matcher = \(x : xs) -> x
 
-f1 (a, b) = a 
+f1 (a, b) = a
