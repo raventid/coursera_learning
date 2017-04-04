e71bea21dedb21f0da1939056ec19e4b8e3ecbc6
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-04-04 20:26:22 +0300

    updates for ch6/ord

--- haskell_experiments/chapter6/ord_worksheet.hs
+++ haskell_experiments/chapter6/ord_worksheet.hs
@@ -1,8 +1,11 @@
 module Worksheet where
 
-data DayOfWeek = 
+
+data DayOfWeek =
   Mon | Tue | Weds | Thu | Fri | Sat | Sun
-  deriving (Ord, Show, Eq)
+  deriving(Show, Eq)
+  -- deriving (Ord, Show, Eq)
+  -- If you need to automatically implement Ord, this is the way to go.
 
 instance Ord DayOfWeek where
   compare Fri Fri = EQ
