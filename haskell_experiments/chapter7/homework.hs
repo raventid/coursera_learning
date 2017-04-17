866b3d65ee3f7a58e7989cd5b3affc30bb531e43
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-04-17 20:32:41 +0300

    filling homework on ch7 next one

--- haskell_experiments/chapter7/homework.hs
+++ haskell_experiments/chapter7/homework.hs
@@ -10,3 +10,16 @@ tensDigit1 x = f2 $ f1 $ x
   where f1 = fst . f
         f2 = snd . f
         f = (`divMod` 10)
+
+foldBool :: a -> a -> Bool -> a
+foldBool a b c = a
+
+foldBool2 :: a -> a -> Bool -> a
+foldBool2 x y c
+  | c == True = x
+  | c == False = y
+
+foldBool3 :: a -> a -> Bool -> a
+foldBool3 x y c = case(c) of
+  (True) -> x
+  (False) -> y
