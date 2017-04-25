c9b081b817327eaad5e8740783762614e44c7d30
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-04-25 21:23:57 +0300

    minor rewrite for m91 function ch8

--- haskell_experiments/chapter8/worksheet.hs
+++ haskell_experiments/chapter8/worksheet.hs
@@ -31,7 +31,7 @@ summator n = go n 0
          | otherwise = go (n - 1) (acc + n)
 
 multiplicator :: Integral a => a -> a -> a
-multiplicator x y = go x y 0
+multiplicator x y = go x y 0 
   where go x y res
          | y == 0 = res
          | otherwise = go x (y - 1) (res + x)
@@ -39,4 +39,4 @@ multiplicator x y = go x y 0
 -- McCarthy function
 m91 n
  | n > 100 = n - 10
- | n <= 100 = m91(m91(n+11))
+ | n <= 100 = m91 . m91 . (+11) $ n
