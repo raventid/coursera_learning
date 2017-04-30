c5d4a7026b6340e9d7c30a9205b2c12ff954b38e
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-04-30 19:41:23 +0300

    whitespaces for ch9

--- haskell_experiments/chapter9/worksheet.hs
+++ haskell_experiments/chapter9/worksheet.hs
@@ -1,6 +1,7 @@
 module Worksheet where
 
 -- List datatype in Haskell defined like this
+-- [] is a sum type, but a : [a] is a product type.
 -- data [] a = [] | a : [a]
 -- This (:) also called product
 
@@ -14,7 +15,7 @@ safeTail (x:[]) = Nothing
 safeTail (_:xs) = Just xs
 
 enumFromTo' :: Enum a => a -> a -> [a]
-enumFromTo' begin end = go begin end 
+enumFromTo' begin end = go begin end
   where go b e
          | f b > f e = []
          | f b == f e = [b]
@@ -25,10 +26,10 @@ eftBool :: Bool -> Bool -> [Bool]
 eftBool a b = [a,b]
 
 eftOrd :: Ordering -> Ordering -> [Ordering]
-eftOrd = enumFromTo' 
+eftOrd = enumFromTo'
 
 eftInt :: Int -> Int -> [Int]
-eftInt = enumFromTo' 
+eftInt = enumFromTo'
 
 eftChar :: Char -> Char -> [Char]
 eftChar = enumFromTo'
@@ -42,17 +43,17 @@ comprehendedList = [ x^2 | x <- [1..10] ]
 myLines :: String -> [String]
 myLines "" = []
 myLines s = (takeWhile f s) : (myLines $ drop 1 $ dropWhile f s)
-  where f = (/=' ') 
+  where f = (/=' ')
 
 myFilter :: String -> [String]
-myFilter s = [filtered | filtered <- myLines s, not $ filtered `elem` toRemove] 
+myFilter s = [filtered | filtered <- myLines s, not $ filtered `elem` toRemove]
   where toRemove = ["the","a","an"]
 
 myFilter' :: String -> [String]
 myFilter' = filter (\w -> not $ elem w ["the","a","an"]) . myLines
 
 zip' :: [a] -> [b] -> [(a,b)]
-zip' (x:[])(y:_) = (x,y) : []  
+zip' (x:[])(y:_) = (x,y) : [] 
 zip' (x:_)(y:[]) = (x,y) : []
 zip' (x:xs)(y:ys) = (x,y) : zip' xs ys
 
@@ -62,8 +63,8 @@ zip'' _ _ = []
 
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' f (x:xs)(y:ys) = f x y : zipWith' f xs ys
-zipWith' _ _ _ = [] 
+zipWith' _ _ _ = []
 
-zip''' :: [a] -> [b] -> [(a,b)] 
-zip''' = zipWith' f 
-  where f x y = (x,y) 
+zip''' :: [a] -> [b] -> [(a,b)]
+zip''' = zipWith' f
+  where f x y = (x,y)
