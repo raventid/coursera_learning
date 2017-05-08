b7da3e714a3ff71414049be0e7ebe3777b58aaf6
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-05-08 17:59:11 +0300

    update exers for ch9

--- haskell_experiments/chapter9/exers.hs
+++ haskell_experiments/chapter9/exers.hs
@@ -10,11 +10,11 @@ upperFirst (x:xs) = toUpper x : xs
 
 upperAll :: String -> String
 upperAll (x:xs) = toUpper x : upperAll xs
-upperAll _ = [] 
+upperAll _ = []
 
 -- just to get it clear
 test' :: String -> String
-test' (x:[]) = "tail is empty" -- One letter in string, if empty string we cannot pattern match  : [] 
+test' (x:[]) = "tail is empty" -- One letter in string, if empty string we cannot pattern match  : []
 test' (x:xs) = "x is " ++ " xs is " ++ xs -- At least two letters in string
 test' _ = "default" -- empty string is left
 
@@ -32,16 +32,16 @@ myAnd' [] = True
 myAnd' (x:xs) = x && myAnd xs
 
 myOr :: [Bool] -> Bool
-myOr (x:xs) = if x then True else myOr xs  
-myOr _  = False 
+myOr (x:xs) = if x then True else myOr xs
+myOr _  = False
 
 myAny :: (a -> Bool) -> [a] -> Bool
-myAny f (x:xs) = if f x then True else myAny f xs 
-myAny _ _ = False 
+myAny f (x:xs) = if f x then True else myAny f xs
+myAny _ _ = False
 
 myElem :: Eq a => a -> [a] -> Bool
 myElem _ [] = False
-myElem e (x:xs) = if e == x then True else myElem e xs  
+myElem e (x:xs) = if e == x then True else myElem e xs
 
 myElem' :: Eq a => a -> [a] -> Bool
 myElem' = \x -> myAny (== x)
@@ -56,23 +56,23 @@ squish [] = []
 squish (x:xs) = x ++ squish xs
 
 squishMap :: (a -> [b]) -> [a] -> [b]
-squishMap _ [] = [] 
-squishMap f (x:xs) = f x ++ squishMap f xs 
+squishMap _ [] = []
+squishMap f (x:xs) = f x ++ squishMap f xs
 
 squishAgain :: [[a]] -> [a]
 squishAgain = squishMap (\x -> x)
 
 myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
-myMaximumBy _ (x:[]) = x 
-myMaximumBy f (x:xs) =
-  let max = myMaximumBy f xs  
+myMaximumBy _ (x:[]) = x
+myMaximumBy f (x:xs)
+  let max = myMaximumBy f xs
   in case f max x of
-       GT -> max 
-       LT -> x 
-       EQ -> x 
+       GT -> max
+       LT -> x
+       EQ -> x
 
 myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
-myMinimumBy _ (x:[]) = x 
+myMinimumBy _ (x:[]) = x
 myMinimumBy f (x:xs) =
   let min = myMinimumBy f xs
   in case f min x of
@@ -85,5 +85,3 @@ myMaximum = myMaximumBy compare
 
 myMinimum :: (Ord a) => [a] -> a
 myMinimum = myMinimumBy compare
-
-
