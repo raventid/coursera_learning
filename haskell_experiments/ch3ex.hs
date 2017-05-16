3cb7193ab3331137163204f49c8e4a9d193238f8
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-05-16 18:12:54 +0300

    review ch3

--- haskell_experiments/ch3ex.hs
+++ haskell_experiments/ch3ex.hs
@@ -2,12 +2,12 @@ module Ch3Ex where
 
 -- thirdLetter :: String -> Char
 -- thirdLetter x = head (drop 2 x)
--- 
+--
 -- letterIndex :: Int -> Char
--- letterIndex x = drop (x-1) 
+-- letterIndex x = drop (x-1)
 
 rvrs :: String -> String
-rvrs x = concat [(drop 9 x), " is ", (take 5 x)] 
+rvrs x = concat [(drop 9 x), " is ", (take 5 x)]
 
 main :: IO ()
 main = print (rvrs "Curry is awesome")
