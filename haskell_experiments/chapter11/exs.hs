29502cf7e64c2fe4e1adfa91208106cdfdfce855
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-06-27 16:58:14 +0300

    add exercise about normal form

--- haskell_experiments/chapter11/exs.hs
+++ haskell_experiments/chapter11/exs.hs
@@ -3,3 +3,16 @@ module Exs where
 data Example = MakeExample deriving Show
 
 data SpecialExample = MakeSpecialExample Int deriving Show
+
+-- What is the normal form of garden?
+data FlowerType = Gardenia
+  | Daisy
+  | Rose
+  | Lilac
+  deriving Show
+
+type Gardener = String
+
+data Garden =
+  Garden Gardener FlowerType
+  deriving Show
