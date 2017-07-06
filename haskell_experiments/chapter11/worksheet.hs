37fe9cc443af54da940a4db77ad227368f39ed57
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-07-06 22:24:40 +0300

    add quontified function definitions and realization

--- haskell_experiments/chapter11/worksheet.hs
+++ haskell_experiments/chapter11/worksheet.hs
@@ -212,3 +212,73 @@ allLanguages = [ Haskell
 -- I wanna work with Haskell full time!!!
 allProgrammers :: [Programmer]
 allProgrammers = [Programmer a b | a <- allOperatingSystems, b <- allLanguages]
+
+-- builder pattern? Look at this ->
+data ThereYet =
+  There Integer Float String Bool
+  deriving (Eq, Show)
+
+nope :: Float -> String -> Bool -> ThereYet
+nope = There 10
+
+notYet :: String -> Bool -> ThereYet
+notYet = nope 25.5
+
+notQuite :: Bool -> ThereYet
+notQuite = notYet "woohoo"
+
+yussss :: ThereYet
+yussss = notQuite True
+
+
+data Quantum =
+    Yes
+    |No
+    | Both
+    deriving (Eq, Show)
+
+-- `->` function is exponential
+-- Let's write all possible values of this:
+-- Quantum has cardinality 3 and Bool -2. So the result is 2^3 possible functions.
+convert1 :: Quantum -> Bool
+convert1 Yes = True
+convert1 No = True
+convert1 Both = True
+
+convert2 :: Quantum -> Bool
+convert2 Yes = True
+convert2 No = True
+convert2 Both = False
+
+convert3 :: Quantum -> Bool
+convert3 Yes = True
+convert3 No = False
+convert3 Both = True
+
+convert4 :: Quantum -> Bool
+convert4 Yes = True
+convert4 No = False
+convert4 Both = False
+
+convert5 :: Quantum -> Bool
+convert5 Yes = False
+convert5 No = True
+convert5 Both = True
+
+convert6 :: Quantum -> Bool
+convert6 Yes = False
+convert6 No = True
+convert6 Both = False
+
+convert7 :: Quantum -> Bool
+convert7 Yes = False
+convert7 No = False
+convert7 Both = True
+
+convert8 :: Quantum -> Bool
+convert8 Yes = False
+convert8 No = False
+convert8 Both = False
+
+-- Polymorphic product type.
+data Silly a b c d = MkSilly a b c d deriving Show
