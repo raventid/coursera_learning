c26381f50e8dbb6cc0135d73a21eaeee57593a87
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-07-07 23:17:03 +0300

    whitespaces for cesar cipher

--- haskell_experiments/common_chapters/cipher.hs
+++ haskell_experiments/common_chapters/cipher.hs
@@ -3,25 +3,25 @@
 module Cipher where
 import Data.Char
 
-transform :: Char -> Int -> Char 
-transform c n = chr $ (+) (denormalizeLetterPosition c) $ findPosition $ (+) (normalizeLetterPosition c) n 
+transform :: Char -> Int -> Char
+transform c n = chr $ (+) (denormalizeLetterPosition c) $ findPosition $ (+) (normalizeLetterPosition c) n
 
 findPosition :: Int -> Int
-findPosition p = p `mod` 26 
+findPosition p = p `mod` 26
 
 normalizeLetterPosition :: Char -> Int
 normalizeLetterPosition c
-  | c `elem` ['A'..'Z'] = ord c - ord 'A' 
+  | c `elem` ['A'..'Z'] = ord c - ord 'A'
   | c `elem` ['a'..'z'] = ord c - ord 'a'
   | otherwise = ord c 
 
 denormalizeLetterPosition :: Char -> Int
 denormalizeLetterPosition c
-  | c `elem` ['A'..'Z'] = ord 'A'  
+  | c `elem` ['A'..'Z'] = ord 'A'
   | c `elem` ['a'..'z'] = ord 'a'
-  | otherwise = ord c 
+  | otherwise = ord c
 
 caesar :: String -> Int -> String
 caesar (x:xs) n = transform x n : caesar xs n
-caesar _ _ = [] 
+caesar _ _ = []
 
