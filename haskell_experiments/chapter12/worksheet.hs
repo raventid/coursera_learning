375fd2275206a8532743c0df8f38750785379c4d
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-07-12 15:39:52 +0300

    first exercises for ch12

--- haskell_experiments/chapter12/worksheet.hs
+++ haskell_experiments/chapter12/worksheet.hs
@@ -85,11 +85,22 @@ notThe str = case str /= "the" of
 -- Well it's not really fare cause it's not recursive and uses intercalate
 -- This realization even makes use of Maybe quqestionable :)
 replaceThe :: String -> String
-replaceThe str = intercalate " " $ map (f . notThe) $ splitBy ' ' str
+replaceThe str = intercalate " " $ map (f . notThe) $ words str
   where f (Just str) = str
         f (Nothing) = "a"
 
-splitBy :: (Foldable b, Eq a) => a -> b a -> [[a]]
-splitBy delimiter = foldr f [[]]
-            where f c l@(x:xs) | c == delimiter = []:l
-                               | otherwise = (c:x):xs
+countTheBeforeVowel :: String -> Int
+countTheBeforeVowel str = go 0 $ words str
+  where isVowelFirstLetter = (`elem` "aeiou") . head
+        go count (x:xs)
+          | x == "the" && isVowelFirstLetter (head xs) = go (count + 1) xs
+          | otherwise = go count xs
+        go count _ = count
+
+countVowels :: String -> Integer
+countVowels str = go 0 str
+  where isVowel = (`elem` "aeiou")
+        go count (x:xs)
+          | isVowel(x) = go (count + 1) xs
+          | otherwise = go count xs
+        go count _ = count
