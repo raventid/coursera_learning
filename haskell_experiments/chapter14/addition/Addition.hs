b07d7b72503b267353b0355400736ed3286105ce
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-07-20 17:35:23 +0300

    add QuickCheck experiment to ch14

--- haskell_experiments/chapter14/addition/Addition.hs
+++ haskell_experiments/chapter14/addition/Addition.hs
@@ -1,12 +1,19 @@
 module Addition where
 
 import Test.Hspec
+import Test.QuickCheck
+
+trivialInt :: Gen Int
+trivialInt = return 1
+
+oneThroughThree :: Gen Int
+oneThroughThree = elements [1, 2, 3]
 
 sayHello :: IO ()
 sayHello = putStrLn "Hello!!!"
 
 summator :: (Eq a, Num a) => a -> a -> a
-summator a 0 = 0
+summator _ 0 = 0
 summator a b = a + summator a (b - 1)
 
 main :: IO ()
@@ -17,7 +24,11 @@ main = hspec $ do
     it "2 + 2 is equal to 4" $ do
       (2 + 2) `shouldBe` 4
 
-
   describe "summator" $ do
     it "takes 2 and 10 and returns 20" $ do
       summator 2 10 `shouldBe` 20
+
+  describe "first quickCheck" $ do
+    it "x + 1 is always greater than x" $ do
+      property $ \x -> x + 1 > (x :: Int)
+
