bbada17e2c8bcc4d0d4753fa4c5e396ec249c3cf
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-07-21 19:21:03 +0300

    some arbitrary + gen code

--- haskell_experiments/chapter14/addition/Addition.hs
+++ haskell_experiments/chapter14/addition/Addition.hs
@@ -9,6 +9,37 @@ trivialInt = return 1
 oneThroughThree :: Gen Int
 oneThroughThree = elements [1, 2, 3]
 
+genBool :: Gen Bool
+genBool = choose (False, True)
+
+genBool' :: Gen Bool
+genBool' = elements [False, True]
+
+genOrdering :: Gen Ordering
+genOrdering = elements [LT, EQ, GT]
+
+genChar :: Gen Char
+genChar = elements ['a'..'z']
+
+genTuple :: (Arbitrary a, Arbitrary b) => Gen(a, b)
+genTuple = do
+  a <- arbitrary
+  b <- arbitrary
+  return (a, b)
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c) => Gen(a, b, c)
+genThreeple = do
+  a <- arbitrary
+  b <- arbitrary
+  c <- arbitrary
+  return (a, b, c)
+
+genEither :: (Arbitrary a, Arbitrary b) => Gen(Either a b)
+genEither = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [Left a, Left b]
+
 sayHello :: IO ()
 sayHello = putStrLn "Hello!!!"
 
