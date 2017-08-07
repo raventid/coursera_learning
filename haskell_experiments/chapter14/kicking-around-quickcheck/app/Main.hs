e8272d94089b446f0165a6f82505ed07a770f877
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-08-07 18:43:10 +0300

    add kicking around quickcheck ch14

---
+++ haskell_experiments/chapter14/kicking-around-quickcheck/app/Main.hs
@@ -0,0 +1,54 @@
+module Main where
+
+import Test.QuickCheck
+
+-- Trivial
+data Trivial =
+  Trivial
+  deriving (Eq, Show)
+
+trivialGen :: Gen Trivial
+trivialGen = return Trivial
+
+instance Arbitrary Trivial where
+  arbitrary = trivialGen
+
+-- Identity
+data Identity a =
+  Identity a
+  deriving (Eq, Show)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = identityGen
+
+identityGen :: Arbitrary a => Gen (Identity a)
+identityGen = do
+  a <- arbitrary
+  return (Identity a)
+
+identityGenInt :: Gen (Identity Int)
+identityGenInt = identityGen
+
+data Pair a b =
+  Pair a b
+  deriving (Eq, Show)
+
+pairGen :: (Arbitrary a,
+            Arbitrary b) =>
+           Gen (Pair a b)
+pairGen = do
+  a <- arbitrary
+  b <- arbitrary
+  return (Pair a b)
+
+instance (Arbitrary a,
+          Arbitrary b) =>
+         Arbitrary (Pair a b) where
+  arbitrary = pairGen
+
+pairGenIntString :: Gen (Pair Int String)
+pairGenIntString = pairGen
+
+main :: IO ()
+main = do
+  sample trivialGen
