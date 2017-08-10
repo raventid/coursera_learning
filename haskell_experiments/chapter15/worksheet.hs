fa82d945a54748daeca1b87ff010593e0e52da10
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-08-10 19:59:03 +0300

    Monoid and numeric ch15

--- haskell_experiments/chapter15/worksheet.hs
+++ haskell_experiments/chapter15/worksheet.hs
@@ -10,3 +10,10 @@ module Worksheet where
 --   mconcat = foldr mappend mempty
 
 -- List is one of many classes who have Monoid instance.
+-- For list there is a funny thing:
+-- foldr mappend mempty == foldr append []
+
+-- As far as I understand now mempty is a 'Identity'
+
+-- NOTE: Numberic do not have Monoid instance.(it's not comprehensible what operation should it be - summation or multiplication)
+-- i.e. should mappend 1 1 be 1 + 1 or 1 * 1 ? :)
