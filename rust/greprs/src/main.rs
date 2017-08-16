dc67ddb23653535a6d9cdc962652dfc770c69ebe
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-08-16 18:03:56 +0300

    minor tweak for cmd args greprs

--- rust/greprs/src/main.rs
+++ rust/greprs/src/main.rs
@@ -2,5 +2,9 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
