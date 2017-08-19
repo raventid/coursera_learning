0e4f9e9a453b7f69dd9921c12d5e95e1d228a199
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-08-19 13:39:01 +0300

    create config struct greprs

--- rust/greprs/src/main.rs
+++ rust/greprs/src/main.rs
@@ -4,16 +4,28 @@ use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    let config = parse_config(&args);
 
-    let mut f = File::open(filename).expect("file not found");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong while reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
