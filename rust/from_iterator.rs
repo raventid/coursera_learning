b69969a88ed22630ea41aeaa3d7e63e2a71d261a
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-08-27 00:03:38 +0300

    rust from-iter

---
+++ rust/from_iterator.rs
@@ -0,0 +1,7 @@
+// This is very important to tell about, this is the idea of iteration:
+// trait FromIterator<A> : Sized {
+//    fn from_iter<T : IntoIterator<Item=A>>(iter: T) -> Self;
+// }
+
+
+// IntoIterator
