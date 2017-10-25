85b0e0ec615e09c17f549011f1964e212a4efd5e
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-10-25 22:53:46 +0300

    add Int8 overflow example

--- haskell_experiments/chapter11/exs.hs
+++ haskell_experiments/chapter11/exs.hs
@@ -1,4 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Exs where
+
+import Data.Int
+
+data NumberOrBool =
+    Numba Int8
+  | BoolyBool Bool
+  deriving (Eq, Show)
+
+-- It's not an error but Haskell will give us warning about overflow.
+someNumberOrBool = Numba (-129)
 
 data Example = MakeExample deriving Show
 
