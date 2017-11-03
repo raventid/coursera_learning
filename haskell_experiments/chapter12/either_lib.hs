8e0b337a36e99af41aafe8a5df9284dc5a10d76e
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-11-03 23:11:03 +0300

    remove undefined case from lefts', GHC agreed with me ch12

--- haskell_experiments/chapter12/either_lib.hs
+++ haskell_experiments/chapter12/either_lib.hs
@@ -1,12 +1,10 @@
 module EitherLib where
 
--- Well... I'm using undefined for real. This is the worst thing I've ever done in Haskell ;)
 lefts' :: [Either a b] -> [a]
 lefts' = foldr (\x acc -> if isLeft x then (left x):acc else acc) []
   where isLeft (Left a) = True
         isLeft _ = False
         left (Left a) = a
-        left _ = undefined
 
 -- Too much repetition here. Should I move it to foldr somehow?
 rights' :: [Either a b] -> [b]
@@ -14,7 +12,6 @@ rights' = foldr (\x acc -> if isRight x then (right x):acc else acc) []
   where isRight (Right a) = True
         isRight _ = False
         right (Right a) = a
-        right _ = undefined
 
 partitionEithers' :: [Either a b] -> ([a], [b])
 partitionEithers' xs = (lefts' xs, rights' xs)
