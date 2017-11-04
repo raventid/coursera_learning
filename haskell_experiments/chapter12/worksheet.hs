984acc8eeaf3e9c50c9b4fcdcd3acdf3f452053c
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-11-04 23:56:12 +0300

    remove exercises from worksheet ch12

--- haskell_experiments/chapter12/worksheet.hs
+++ haskell_experiments/chapter12/worksheet.hs
@@ -1,6 +1,5 @@
 module Chapter12 where
 
-import Data.List
 
 -- Let's start with Maybe
 ifEvenAdd2 :: Integer -> Maybe Integer
@@ -77,31 +76,3 @@ data ThisIsTypeConstructor a = Blah | Blah' | Blah'' a
 -- # are raw pointers and native types
 
 -- Newtypes are very special in this case, they are unlifted, but in the same time, they are *.
-
-notThe :: String -> Maybe String
-notThe str = case str /= "the" of
-  True -> Just str
-  False -> Nothing
-
--- Well it's not really fare cause it's not recursive and uses intercalate
--- This realization even makes use of Maybe questionable :)
-replaceThe :: String -> String
-replaceThe str = intercalate " " $ map (f . notThe) $ words str
-  where f (Just str) = str
-        f (Nothing) = "a"
-
-countTheBeforeVowel :: String -> Int
-countTheBeforeVowel str = go 0 $ words str
-  where isVowelFirstLetter = (`elem` "aeiou") . head
-        go count (x:xs)
-          | x == "the" && isVowelFirstLetter (head xs) = go (count + 1) xs
-          | otherwise = go count xs
-        go count _ = count
-
-countVowels :: String -> Integer
-countVowels str = go 0 str
-  where isVowel = (`elem` "aeiou")
-        go count (x:xs)
-          | isVowel(x) = go (count + 1) xs
-          | otherwise = go count xs
-        go count _ = count
