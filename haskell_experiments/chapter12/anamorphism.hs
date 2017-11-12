8f390d0326028217d04780fcb5ff6880bd90080a
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-11-12 23:47:09 +0300

    self explanation about stream in comment ch12

--- haskell_experiments/chapter12/anamorphism.hs
+++ haskell_experiments/chapter12/anamorphism.hs
@@ -1,5 +1,11 @@
 module Anamorphism where
 
+-- As far as I understand this it is like stream in Racket:
+-- (define ones (lambda () (cons 1 ones)))
+-- But Racket can easily store different elements in list (dynamic types), I think that Haskell does
+-- the next thing. GHC looks at this code and thinks - Hmmm, he asks me to build a list, but it's an
+-- infinite recursion, oh I'm lazy! I can typecheck this and if thats write I can calculate tail only
+-- when it's needed. The question is how does it know how to finish the list with ` lastElement : []`?
 myIterate :: (a -> a) -> a -> [a]
 myIterate f a = a : myIterate f (f a)
 
