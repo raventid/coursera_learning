2cd8a77f2d77754db61b2c87265d9c691572fd57
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-11-15 23:14:41 +0300

    grief note about tree visualizing ch12

--- haskell_experiments/chapter12/binary_tree.hs
+++ haskell_experiments/chapter12/binary_tree.hs
@@ -11,8 +11,13 @@ unfold f b = case f b of
   Nothing -> Leaf
 
 -- TODO: it would be very nice to add funny pretty printing like
---     0
---   /   \
---  1     1
+--          0
+--     /          \
+--    1            1
+--   /  \        /  \
+--  2    2      2    2
+-- / \  / \    / \   / \
+-- 3 3 3   3   3  3  3  3
+-- But it seems to be a bit harder for deep trees. I'm not sure I know how to visualize deep tree this way :(
 treeBuild :: Integer -> BinaryTree Integer
 treeBuild = unfold (\i -> if i == 0 then Nothing else Just(i-1, i, i-1)
