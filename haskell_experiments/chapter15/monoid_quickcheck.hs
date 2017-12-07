8a35b250592cdba53cf5bb20b4dc5cde7ecb1dde
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-12-08 00:03:42 +0300

    trying to solve my problem with dependencies, not yet but WIP

---
+++ haskell_experiments/chapter15/monoid_quickcheck.hs
@@ -0,0 +1,9 @@
+module MonoidQuickcheck where
+
+import Data.Monoid
+import Test.QuickCheck
+
+monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
+monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+-- quickCheck (monoidAssoc :: String -> String -> String -> Bool)
