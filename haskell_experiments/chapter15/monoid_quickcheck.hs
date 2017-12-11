f9f0046b0c4724477ef364cf27ae300b519c9087
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-12-11 21:14:36 +0300

    remove unused files

--- haskell_experiments/chapter15/monoid_quickcheck.hs
+++
@@ -1,9 +0,0 @@
-module MonoidQuickcheck where
-
-import Data.Monoid
-import Test.QuickCheck
-
-monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
-monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
-
--- quickCheck (monoidAssoc :: String -> String -> String -> Bool)
