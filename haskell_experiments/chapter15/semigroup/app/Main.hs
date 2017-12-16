489d0ba74b2ffc23e3365fd94461122b4a65bd05
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-12-16 21:13:34 +0300

    7 segmigroup exercises

--- haskell_experiments/chapter15/semigroup/app/Main.hs
+++ haskell_experiments/chapter15/semigroup/app/Main.hs
@@ -1,12 +1,183 @@
 module Main where
 
+import qualified Data.Monoid as M
 import Data.Semigroup
 import Data.List.NonEmpty
+import Test.QuickCheck
 
 -- We can create custom symbolic constructors!
 -- But this symbolic constructor cannot be prefix, only infix :(
 data Q =
   Int :!!: String
+
+-- Let's start exercises part:
+
+-- Our semigroup rule we will use for every proof:
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c =
+ (a <> (b <> c)) == ((a <> b) <> c)
+
+
+-- 1.
+data Trivial = Trivial deriving (Eq, Show)
+
+-- This is one is extremely straightforward
+instance Semigroup Trivial where
+  _ <> _ = Trivial
+
+instance Arbitrary Trivial where
+  arbitrary = return Trivial
+
+type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+runExercise1Spec :: IO ()
+runExercise1Spec = quickCheck (semigroupAssoc :: TrivAssoc)
+
+-- 2.
+newtype Identity a = Identity a deriving (Eq, Show)
+
+instance (Semigroup a) => Semigroup (Identity a) where
+  (Identity x) <> (Identity y) = Identity (x <> y)
+
+instance (Arbitrary a) => Arbitrary (Identity a) where
+  arbitrary = do
+    x <- arbitrary
+    return (Identity x)
+
+type IdentAssoc = Identity String -> Identity String -> Identity String -> Bool
+
+runExercise2Spec :: IO ()
+runExercise2Spec = quickCheck (semigroupAssoc :: IdentAssoc)
+
+-- 3.
+data Two a b = Two a b deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+  (Two x y) <> (Two x' y') = Two (x <> x') (y <> y')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    return (Two x y)
+
+type TwoAssoc = Two String (Sum Int) -> Two String (Sum Int) -> Two String (Sum Int) -> Bool
+
+runExercise3Spec :: IO ()
+runExercise3Spec = quickCheck (semigroupAssoc :: TwoAssoc)
+
+-- 4.
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c) => Semigroup (Three a b c) where
+  (Three x y z) <> (Three x' y' z') = Three (x <> x') (y <> y') (z <> z')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    z <- arbitrary
+    return (Three x y z)
+
+type ThreeAssoc =
+  Three String (Sum Int) (Product Int)
+  -> Three String (Sum Int) (Product Int)
+  -> Three String (Sum Int) (Product Int)
+  -> Bool
+
+runExercise4Spec :: IO()
+runExercise4Spec = quickCheck (semigroupAssoc :: ThreeAssoc)
+
+-- 5. It's already boring a bit, but I'll do this.
+data Four a b c d = Four a b c d
+
+instance (Eq a, Eq b, Eq c, Eq d) => Eq(Four a b c d) where
+  (==) (Four x y z w) (Four x' y' z' w') = x == x' && y == y' && z == z' && w == w'
+
+instance (Show a, Show b, Show c, Show d) => Show(Four a b c d) where
+  show (Four x y z w) =
+    "Four " ++ show x ++ " " ++ show y ++ " " ++ show z ++ " " ++ show w
+
+instance (Semigroup a, Semigroup b, Semigroup c, Semigroup d) => Semigroup(Four a b c d) where
+  (Four x y z w) <> (Four x' y' z' w') = Four (x <> x') (y <> y') (z <> z') (w <> w')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary(Four a b c d) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    z <- arbitrary
+    w <- arbitrary
+    return (Four x y z w)
+
+type FourAssoc =
+  Four String (Sum Int) (Product Int) String
+  -> Four String (Sum Int) (Product Int) String
+  -> Four String (Sum Int) (Product Int) String
+  -> Bool
+
+runExercise5Spec :: IO ()
+runExercise5Spec = quickCheck (semigroupAssoc :: FourAssoc)
+
+-- 6.
+newtype BoolConj = BoolConj Bool deriving(Eq, Show)
+
+instance Semigroup(BoolConj) where
+  (BoolConj True) <> (BoolConj True) = BoolConj True
+  _ <> _ = BoolConj False
+
+instance Arbitrary(BoolConj) where
+  arbitrary = do
+    x <- arbitrary
+    return (BoolConj x)
+
+type BoolConjAssoc = BoolConj -> BoolConj -> BoolConj -> Bool
+
+runExercise6Spec :: IO ()
+runExercise6Spec = quickCheck (semigroupAssoc :: BoolConjAssoc)
+
+-- 7.
+newtype BoolDisj = BoolDisj Bool deriving (Eq, Show)
+
+instance Semigroup(BoolDisj) where
+  (BoolDisj True) <> (BoolDisj True) = BoolDisj True
+  (BoolDisj True) <> (BoolDisj False) = BoolDisj True
+  (BoolDisj False) <> (BoolDisj True) = BoolDisj True
+  _ <> _ = BoolDisj False
+
+instance Arbitrary(BoolDisj) where
+  arbitrary = do
+    x <- arbitrary
+    return (BoolDisj x)
+
+type BoolDisjAssoc = BoolDisj -> BoolDisj -> BoolDisj -> Bool
+
+runExercise7Spec :: IO ()
+runExercise7Spec = quickCheck (semigroupAssoc :: BoolDisjAssoc)
+
+-- 8.
+data Or a b =
+    Fst a
+  | Snd b
+  deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup(Or a b) where
+  (Fst x) <> (Fst y) = Fst y
+  (Fst x) <> (Snd y) = Snd y
+  (Snd x) <> (Fst y) = Snd x
+  (Snd x) <> (Snd y) = Snd y
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary(Or a b) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    oneof [ return $ Fst x
+          , return $ Snd y ]
+
+
+type OrAssoc = Or String (Sum Int) -> Or String (Sum Int) -> Or String (Sum Int) -> Bool
+
+runExercise8Spec :: IO ()
+runExercise8Spec = quickCheck (semigroupAssoc :: OrAssoc)
 
 main :: IO ()
 main = putStrLn "Stub main function. Write your code in main."
