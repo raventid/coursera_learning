31400cd348b654a8bb8b13642f02214cd2d482e7
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-01-07 21:21:00 +0300

    custom implementation for bind

--- haskell_experiments/chapter18/worksheet.hs
+++ haskell_experiments/chapter18/worksheet.hs
@@ -1,1 +1,9 @@
 module Chapter18 where
+
+import Control.Monad (join)
+import Control.Applicative
+
+
+-- keep in mind this is (>>=) flipped
+bind :: Monad m => (a -> m b) -> m a -> m b
+bind f xs = join $ fmap f xs
