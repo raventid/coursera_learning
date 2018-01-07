461dd5f734c92400b7a905269fb51a10385d42ae
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-01-07 18:47:00 +0300

    checkers tests for simple applicatives

--- haskell_experiments/chapter17/applicative/app/Main.hs
+++ haskell_experiments/chapter17/applicative/app/Main.hs
@@ -128,6 +128,155 @@ testApplicativeForValidation = do
   quickBatch (applicative (undefined :: Validation String (String, String, String)))
 
 
+-- Chapter exercises
+-- 1.
+data Pair a = Pair a a deriving (Eq, Show)
+
+instance Functor(Pair) where
+  fmap f (Pair a a') = Pair (f a) (f a')
+
+instance Applicative(Pair) where
+  pure a = Pair a a
+
+  (<*>) (Pair f f') (Pair x x') = Pair (f x) (f' x')
+
+instance (Arbitrary a) => Arbitrary(Pair a) where
+  arbitrary = do
+    a <- arbitrary
+    a' <- arbitrary
+    return $ Pair a a'
+
+instance (Eq a) => EqProp(Pair a) where
+  (=-=) = eq
+
+-- TODO: I'll use this approach with undefined everywhere, but I should find the way out.
+testApplicativeForPair :: IO ()
+testApplicativeForPair =
+  quickBatch (applicative (undefined :: Pair (String, String, String)))
+
+-- 2.
+data Two a b = Two a b deriving (Eq, Show)
+
+instance Functor(Two a) where
+  fmap f (Two a b) = Two a (f b)
+
+instance (Monoid a) => Applicative(Two a) where
+  pure b = Two mempty b
+
+  (<*>) (Two a f) (Two a' b) = Two (a <> a') (f b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary(Two a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    return $ Two a b
+
+instance (Eq a, Eq b) => EqProp(Two a b) where
+  (=-=) = eq
+
+testApplicativeForTwo :: IO ()
+testApplicativeForTwo = quickBatch (applicative (undefined :: Two String (String, String, String)))
+
+-- 3.
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance Functor(Three a b) where
+  fmap f (Three a b c) = Three a b (f c)
+
+instance (Monoid a, Monoid b) => Applicative(Three a b) where
+  pure a = Three mempty mempty a
+
+  (<*>) (Three a b f) (Three a' b' x) = Three (a <> a') (b <> b') (f x)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary(Three a b c) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return $ Three a b c
+
+instance (Eq a, Eq b, Eq c) => EqProp(Three a b c) where
+  (=-=) = eq
+
+testApplicativeForThree :: IO ()
+testApplicativeForThree = quickBatch (applicative (undefined :: Three String String (String, String, String)))
+
+-- 4.
+data Three' a b = Three' a b b deriving (Eq, Show)
+
+instance Functor(Three' a) where
+  fmap f (Three' a b b') = Three' a (f b) (f b')
+
+instance (Monoid a) => Applicative(Three' a) where
+  pure a = Three' mempty a a
+
+  (<*>) (Three' a f f') (Three' a' x x') = Three' (a <> a') (f x) (f' x')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary(Three' a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    b' <- arbitrary
+    return $ Three' a b b'
+
+instance (Eq a, Eq b) => EqProp(Three' a b) where
+  (=-=) = eq
+
+testApplicativeForThreePrime :: IO ()
+testApplicativeForThreePrime =
+  quickBatch (applicative (undefined :: (Three' (String,String,String) (String, String, String))))
+
+-- 5.
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance Functor(Four a b c) where
+  fmap f (Four a b c d) = Four a b c (f d)
+
+instance (Monoid a, Monoid b, Monoid c) => Applicative(Four a b c) where
+  pure a = Four mempty mempty mempty a
+
+  (<*>) (Four a b c f) (Four a' b' c' x) = Four (a <> a') (b <> b') (c <> c') (f x)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary(Four a b c d) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    d <- arbitrary
+    return $ Four a b c d
+
+instance (Eq a, Eq b, Eq c, Eq d) => EqProp(Four a b c d) where
+  (=-=) = eq
+
+testApplicativeForFour :: IO ()
+testApplicativeForFour = quickBatch (applicative (undefined :: Four (String, String, String) (String, String, String) (String, String, String) (String, String, String)))
+
+-- 6.
+data Four' a b = Four' a a a b deriving (Eq, Show)
+
+instance Functor(Four' a) where
+  fmap f (Four' a a' a'' b) = Four' a a' a'' (f b)
+
+instance (Monoid a) => Applicative(Four' a) where
+  pure a = Four' mempty mempty mempty a
+
+  (<*>) (Four' a a' a'' f) (Four' b b' b'' x) = Four' (a <> b) (a' <> b') (a'' <> b'') (f x)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary(Four' a b) where
+  arbitrary = do
+    a   <- arbitrary
+    a'  <- arbitrary
+    a'' <- arbitrary
+    b   <- arbitrary
+    return $ Four' a a' a'' b
+
+instance (Eq a, Eq b) => EqProp(Four' a b) where
+  (=-=) = eq
+
+testApplicativeForFourPrime :: IO ()
+testApplicativeForFourPrime =
+  quickBatch(applicative (undefined :: Four' (String, String, String) (String, String, String)))
+
 -- universal main for every task here (or it might just stay empty)
 main :: IO ()
 main = quickBatch (monoid Twoo)
