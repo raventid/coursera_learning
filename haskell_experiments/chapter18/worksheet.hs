72088e742723faf1e8e16b37aaabccad43e7335b
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-01-08 18:12:59 +0300

    use do notation instead of folding for list operations

--- haskell_experiments/chapter18/worksheet.hs
+++ haskell_experiments/chapter18/worksheet.hs
@@ -7,3 +7,11 @@ import Control.Applicative
 -- Keep in mind this is (>>=) flipped
 bind :: Monad m => (a -> m b) -> m a -> m b
 bind f xs = join $ fmap f xs
+
+
+twiceWhenEven :: [Integer] -> [Integer]
+twiceWhenEven xs = do
+  x <- xs
+  if even x
+    then [x*x, x*x]
+    else []
