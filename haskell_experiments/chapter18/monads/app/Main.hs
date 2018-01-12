768bbee506d42dd8a9573d4a4e6fb7b392a48e09
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-01-12 09:39:57 +0300

    format monad laws

--- haskell_experiments/chapter18/monads/app/Main.hs
+++ haskell_experiments/chapter18/monads/app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import Lib
+-- Monad laws:
+
+-- 1.
+-- right identity
+-- m >>= return = m
+
+-- 2.
+-- left identity
+-- return x >>= f = fx
+
+-- 3.
+-- Associativity
+-- (m >>= f) >>= g = m >>= (\x -> f x >>= g)
 
 main :: IO ()
-main = someFunc
+main = putStrLn "My favorite stub function for main, the way I like it"
