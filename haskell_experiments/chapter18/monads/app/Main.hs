26495c4ab23c2a10c3e4cc4c8fa5d44171d6e79e
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-01-14 01:42:09 +0300

    bad monad example with checkers

--- haskell_experiments/chapter18/monads/app/Main.hs
+++ haskell_experiments/chapter18/monads/app/Main.hs
@@ -22,5 +22,35 @@ import Test.QuickCheck.Classes
 -- quickBatch (monad [(1, 2, 3)])
 
 
+
+-- Bad monad stuff
+
+data CountMe a = CountMe Integer a deriving (Eq, Show)
+
+instance Functor CountMe where
+  fmap f (CountMe i a) = CountMe (i + 1) (f a)
+
+instance Applicative CountMe where
+  pure = CountMe 0
+  CountMe n f <*> CountMe n' a = CountMe (n + n') (f a)
+
+instance Monad CountMe where
+  return = pure
+  CountMe n a >>= f = let CountMe _ b = f a in CountMe (n+1) b
+
+instance Arbitrary a => Arbitrary (CountMe a) where
+  arbitrary = CountMe <$> arbitrary <*> arbitrary
+
+instance Eq a => EqProp (CountMe a) where
+  (=-=) = eq
+
+badMonadSpec :: IO ()
+badMonadSpec = do
+  let trigger :: CountMe (Int, String, Int)
+      trigger = undefined
+  quickBatch $ functor trigger
+  quickBatch $ applicative trigger
+  quickBatch $ monad trigger
+
 main :: IO ()
 main = putStrLn "My favorite stub function for main, the way I like it"
