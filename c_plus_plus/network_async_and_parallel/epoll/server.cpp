e67d08e0f750f14efab889a64806d4f9f6dee0ce
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-01-27 21:11:25 +0300

    epoll skeleton, not working yet

--- c_plus_plus/network_async_and_parallel/epoll/server.cpp
+++ c_plus_plus/network_async_and_parallel/epoll/server.cpp
@@ -1,5 +1,27 @@
 // Only for Linux :)
+// So we'll use Docker to compile this on OS X.
 #include <iostream>
+#include <algorithm>
+#include <set>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+
+#define MAX_EVENTS 32
+
+int set_nonblock(int fd) {
+  int flags;
+
+  if (-1 == (flags = fcntl(fd, F_GETFL, 0))) {
+    flags = 0;
+  }
+
+  return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
 
 
 // LEVEL TRIGGERED. Notification happens when we have something to read.
@@ -11,5 +33,25 @@
 
 
 int main() {
+
+  int MasterSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+  struct sockaddr_in SockAddr;
+  SockAddr.sin_family = AF_INET;
+  SockAddr.sin_port = htons(12345);
+  SockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  bind(MasterSocket, (struct sockaddr *)(&SockAddr), sizeof(SockAddr));
+
+  set_nonblock(MasterSocket);
+
+  listen(MasterSocket, SOMAXCONN);
+
+  while(true) {
+    for() {
+      
+    }
+  }
+
   return 0;
 }
