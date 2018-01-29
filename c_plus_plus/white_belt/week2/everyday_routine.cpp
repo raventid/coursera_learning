d158bb20bf4abab74147f7df3856b2a7cd31ef2d
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-01-29 22:13:10 +0300

    working routine, but code is reallyyyyyyyyyy terrible

--- c_plus_plus/white_belt/week2/everyday_routine.cpp
+++ c_plus_plus/white_belt/week2/everyday_routine.cpp
@@ -32,8 +32,6 @@
 // Append v2 to v1:
 // v1.insert(end(v1), begin(v2), end(v2));
 
-
-
 // Формат ввода:
 
 // Сначала число операций Q, затем описания операций.
@@ -50,6 +48,35 @@ int main() {
   int Q;
   std::cin >> Q;
 
+  // 1	Январь	31
+  // 2	Февраль	28 (29 — в високосном году)
+  // 3	Март	31
+  // 4	Апрель	30
+  // 5	Май	31
+  // 6	Июнь	30
+  // 7	Июль	31
+  // 8	Август	31
+  // 9	Сентябрь	30
+  // 10	Октябрь	31
+  // 11	Ноябрь	30
+  // 12	Декабрь	31
+
+  // warning here because it's c++11 addition, looks like flycheck uses 98/03.
+  std::vector<int> months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+  std::vector< std::vector< std::vector<std::string> > > calendar;
+  int selected_month = 0;
+
+  for(int i = 0; i < months.size(); i++) {
+    std::vector< std::vector< std::string > > m_data;
+
+    for(int j = 0; j < months[i]; j++) {
+      std::vector<std::string> m = {};
+      m_data.push_back(m);
+    }
+
+    calendar.push_back(m_data);
+  }
 
   for(int i = 0; i < Q; i++) {
     std::string command;
@@ -57,17 +84,106 @@ int main() {
     std::string todoName;
 
     std::cin >> command;
+
     if (command == "ADD") {
       std::cin >> dayNumber;
+      dayNumber--; // I'm starting day with 0;
       std::cin >> todoName;
+
+      calendar[selected_month][dayNumber].push_back(todoName);
     }
+
     if (command == "DUMP") {
       std::cin >> dayNumber;
+      dayNumber--; // I'm starting days with 0.
+      if(!calendar[selected_month][dayNumber].empty()) {
+        std::cout << calendar[selected_month][dayNumber].size() << " ";
+        for(auto task : calendar[selected_month][dayNumber]) {
+          std::cout << task << " ";
+        }
+      } else {
+        std::cout << 0;
+      }
+      std::cout << std::endl;
     }
-    if (command == "NEXT") {
 
+    if (command == "NEXT") {
+      int next_month = selected_month + 1;
+
+      if(selected_month == 11) {
+        next_month = 0;
+      }
+
+      {
+        calendar[next_month].resize(0);
+        std::vector< std::vector< std::string > > m_data;
+
+        for(int j = 0; j < months[next_month]; j++) {
+          std::vector<std::string> m = {};
+          m_data.push_back(m);
+        }
+
+        calendar[next_month] = m_data;
+      }
+
+      int days_in_next = calendar[next_month].size();
+      int days_in_current = calendar[selected_month].size();
+
+      if(days_in_current <= days_in_next) {
+        // just copy everything as it is
+        for(int i = 0; i < days_in_current; i++) {
+          calendar[next_month][i] = calendar[selected_month][i];
+        }
+      } else {
+        // if next is smaller - copy every element until last element in next
+        for(int i = 0; i < days_in_next; i++) {
+          calendar[next_month][i] = calendar[selected_month][i];
+        }
+
+        // after this append tail to last element in our array
+        int difference_in_days = days_in_current - days_in_next;
+
+        for(int i = days_in_next;
+            i < days_in_next + difference_in_days;
+            i++) {
+          // Append v2 to v1:
+          calendar[next_month][days_in_next-1]
+            .insert(end(calendar[next_month][days_in_next-1]),
+                    begin(calendar[selected_month][i]),
+                    end(calendar[selected_month][i]));
+        }
+      }
+
+      selected_month = next_month;
     }
   }
 
   return 0;
 }
+
+
+// INPUT:
+
+// 12
+// ADD 5 Salary
+// ADD 31 Walk
+// ADD 30 WalkPreparations
+// NEXT
+// DUMP 5
+// DUMP 28
+// NEXT
+// DUMP 31
+// DUMP 30
+// DUMP 28
+// ADD 28 Payment
+// DUMP 28
+
+
+// OUTPUT:
+
+// 1 Salary
+// 2 WalkPreparations Walk
+// 0
+// 0
+// 2 WalkPreparations Walk
+// 3 WalkPreparations Walk Payment
