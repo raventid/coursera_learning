9589791b79d21a21961f64e0f0da39208d3b3ecb
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-02-04 22:55:05 +0300

    skeleton for lecture exercise

--- c_plus_plus/white_belt/week4/structure_lecture_title.cpp
+++ c_plus_plus/white_belt/week4/structure_lecture_title.cpp
@@ -1,5 +1,21 @@
-#inlcude <iostream>
+#include <iostream>
+#include <string>
+
+struct LectureTitle {
+public:
+
+private:
+  std::string specialization;
+  std::string course;
+  std::string week;
+};
 
 int main() {
+  LectureTitle title(
+                     Specialization("C++"),
+                     Course("White belt"),
+                     Week("4th")
+                     );
+
   return 0;
 }
