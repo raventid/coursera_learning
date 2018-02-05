2c07ab6f2821c1ed91de422897ef8992f3caac95
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-02-05 11:04:43 +0300

    add kw args emulation

--- c_plus_plus/white_belt/week4/structure_lecture_title.cpp
+++ c_plus_plus/white_belt/week4/structure_lecture_title.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
 #include <string>
 
+
+struct Specialization {
+  std::string value;
+  explicit Specialization(std::string title) {
+    value = title;
+  }
+};
+
+struct Course {
+  std::string value;
+  explicit Course(std::string title) {
+    value = title;
+  }
+};
+
+struct Week {
+  std::string value;
+  explicit Week(std::string number) {
+    value = number;
+  }
+};
+
 struct LectureTitle {
 public:
+  LectureTitle(Specialization s, Course c, Week w) {
+    specialization = s.value;
+    course = c.value;
+    week = w.value;
+  }
 
-private:
+  // had to remove `private:` cause Yandex grader didn't see those fields
   std::string specialization;
   std::string course;
   std::string week;
 };
 
-int main() {
-  LectureTitle title(
-                     Specialization("C++"),
-                     Course("White belt"),
-                     Week("4th")
-                     );
+// int main() {
+//   LectureTitle title(
+//                      Specialization("C++"),
+//                      Course("White belt"),
+//                      Week("4th")
+//                      );
 
-  return 0;
-}
+//   return 0;
+// }
