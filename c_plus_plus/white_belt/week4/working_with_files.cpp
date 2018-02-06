1871a298ccf519d14229828037daffa057807a33
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-02-07 00:01:44 +0300

    add ifstream example

---
+++ c_plus_plus/white_belt/week4/working_with_files.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+
+int main() {
+  std::ifstream input("input.txt");
+  std::string line;
+
+  while(getline(input, line)) {
+    std::cout << line << std::endl;
+  }
+
+  return 0;
+}
