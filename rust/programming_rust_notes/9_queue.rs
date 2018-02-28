a46c357edb0aa005a6746cfc280c060efebf26c2
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-02-28 08:19:35 +0300

    basic queue

---
+++ rust/programming_rust_notes/9_queue.rs
@@ -0,0 +1,18 @@
+pub struct Queue<T> {
+    older: Vec<T>,
+    younger: Vec<T>
+}
+
+impl<T> Queue<T> {
+    pub fn new() -> Queue<T> {
+        Queue { older: Vec::new(), younger: Vec::new() }
+    }
+
+    pub fn push(&mut self, t: T) {
+        self.younger.push(t);
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.younger.is_empty() == self.older.is_empty()
+    }
+}
