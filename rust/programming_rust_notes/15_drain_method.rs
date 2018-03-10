6d7aebcf2d79d20019deca77b8eb04394b2e9bdb
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-03-10 17:16:04 +0300

    small note about `drain` iterator method

---
+++ rust/programming_rust_notes/15_drain_method.rs
@@ -0,0 +1,9 @@
+use std::iter::FromIterator;
+
+fn main() {
+    let mut outer = "Earth".to_string();
+    let inner = String::from_iter(outer.drain(1..4));
+
+    assert_eq!(outer, "Eh");
+    assert_eq!(inner, "art");
+}
