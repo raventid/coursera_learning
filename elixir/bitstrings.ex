9b01dab87fb79192ad7e28f5e5ed39523ceb9e4b
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-03-24 21:53:01 +0300

    typo in bitstrings pattern

--- elixir/bitstrings.ex
+++ elixir/bitstrings.ex
@@ -1,5 +1,5 @@
 defmodule Bitstringer do
-  # Output is a section of one bute(8 bits)
+  # Output is a section of one byte(8 bits)
   # in output they are represented as a decimal number
   def separation do
     <<21421531142142141412::256>>
