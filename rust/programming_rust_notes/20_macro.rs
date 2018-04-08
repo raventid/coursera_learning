475e9dd042c11f4b2c162126f4a510456f2d6afe
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-04-08 12:44:34 +0300

    wip json macro

--- rust/programming_rust_notes/20_macro.rs
+++ rust/programming_rust_notes/20_macro.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+
 macro_rules! assert_special {
     ($left:expr, $right:expr) => ({
         match (&$left, &$right) {
@@ -44,6 +47,17 @@ macro_rules! vector {
 // $( ... );+ Match 1 or more times, separated by semicolons
 
 
+#[derive(Clone, PartialEq, Debug)]
+enum Json {
+    Null,
+    Boolean(bool),
+    Number(f64),
+    String(String),
+    Array(Vec<Json>),
+    Object(Box<HashMap<String, Json>>)
+}
+
+
 fn main() {
     let val = assert_special!("hello", "hello");
     println!("{:?}", val);
@@ -54,4 +68,28 @@ fn main() {
     // third case with trailing comma
     let some_vector_with_trailing_comma = vector![10, 1, 10,];
     println!("{:?}", some_vector_with_trailing_comma);
+
+    // initialize Json structure without a macro
+    let students = Json::Array(vec![
+        Json::Object(Box::new(vec![
+            ("name".to_string(), Json::String("Raventid".to_string())),
+            ("age".to_string(), Json::Number(32.))
+        ].into_iter().collect())),
+        Json::Object(Box::new(vec![
+            ("name".to_string(), Json::String("Raventida".to_string())),
+            ("age".to_string(), Json::Number(26.))
+        ].into_iter().collect()))
+    ]);
+
+    // let's delegate this code generation to macro
+    let students = json!([{
+        "name": "Raventid",
+        "class_of": 32
+    },
+    {
+        "name": "Raventida",
+        "age": 26
+    }]);
+
+    println!("{:?}", students);
 }
