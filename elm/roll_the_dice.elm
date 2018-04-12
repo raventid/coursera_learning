bf9bde6655590010badff67b28648c04b885ee8b
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-04-12 22:15:18 +0300

    elm roll the dice

---
+++ elm/roll_the_dice.elm
@@ -0,0 +1,76 @@
+-- This program is modified version of:
+-- https://guide.elm-lang.org/architecture/effects/random.html
+
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+import Random
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+
+-- MODEL
+
+type alias Model =
+  { dieFace : Int
+  }
+
+
+init : (Model, Cmd Msg)
+init =
+  (Model 1, Cmd.none)
+
+
+
+
+-- UPDATE
+
+type Msg
+  = Roll
+  | RollOutbound
+  | NewFace Int
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Roll ->
+      (model, Random.generate NewFace (Random.int 1 6))
+
+    RollOutbound ->
+      (model, Random.generate NewFace (Random.int 1 20))
+
+    NewFace newFace ->
+      (Model newFace, Cmd.none)
+
+
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h1 [] [
+        --  Pretty ugly. Is it view or update? Definitely it's helper function + some path constant. Or even structure.
+        img [src ("https://www.wpclipart.com/recreation/games/dice/die_face_" ++ (toString model.dieFace) ++ ".png")] []
+      ]
+    , button [ onClick Roll ] [ text "Roll" ]
+    , button [ onClick RollOutbound] [ text "RollOutbound" ]
+    ]
