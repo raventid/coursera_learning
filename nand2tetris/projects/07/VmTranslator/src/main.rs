f1dc9f117d38eb0dac8b77478b4356b758b8c78e
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-05-04 23:06:21 +0300

    basic parser in imperative style, first draft

--- nand2tetris/projects/07/VmTranslator/src/main.rs
+++ nand2tetris/projects/07/VmTranslator/src/main.rs
@@ -3,19 +3,41 @@
 #![allow(dead_code)]
 
 mod parser;
+use parser::Parser;
+
 mod code_writer;
 
 fn main() {
     // BasicTest file to test VmTranslator located here:
     // /Users/julian/Experiments/coursera_learning/nand2tetris/projects/07/MemoryAccess/BasicTest/BasicTest.vm
-    println!("Hello, world!");
     // 1) parser: start parser stream
+
+    let target_input = "/Users/julian/Experiments/coursera_learning/nand2tetris/projects/07/MemoryAccess/BasicTest/BasicTest.vm".to_string();
+
+    let mut parser = Parser::new(target_input);
+
     // 2) code_writer: start outup stream
+    // let code_writer = CodeWriter::new(target_output);
+
+
     // 3) parser: if next_command exist
-    // 4) parser: advance
-    // 4) parser: send me command and token type
-    // 5) code_writer: dispatche type of token and match against machine representation
-    // 6) code_writer: do this until parser: next_command exists in file
+    loop {
+        let (end_of_stream, correct_command) = parser.has_more_commands();
+        if !end_of_stream && correct_command {
+            // 4) parser: advance
+            parser.advance();
+            // 4) parser: send me command and token type
+            let command_type = parser.command_type();
+            println!("{}", parser.current_command.1);
+        }
+
+        if end_of_stream {
+            break
+        }
+        // 5) code_writer: dispatche type of token and match against machine representation
+    }
     // 7) parser: close file
+    // parser.close_stream();
     // 8) code_writer: close file
+    // code_writer.close_stream();
 }
--- nand2tetris/projects/07/VmTranslator/src/parser.rs
+++ nand2tetris/projects/07/VmTranslator/src/parser.rs
@@ -1,11 +1,14 @@
 use std::io::{ BufReader };
 use std::io::prelude::*;
 use std::fs::File;
-use std::result::Result;
-use std::io::Error;
+pub struct Parser {
+    target_descriptor: BufReader<File>,
+    pub current_command: (Command, String),
+    pub next_command: (Command, String)
+}
 
-// All available commands for virtual machine
-enum Command {
+// All available command types for virtual machine
+pub enum Command {
    C_ARITHMETIC,
    C_PUSH,
    C_POP,
@@ -17,30 +20,46 @@ enum Command {
    C_CALL,
 }
 
-// Opens file/stream, make every other component ready to handle it
-fn constructor(input_file : String) -> Result<(), Error> {
-    let f = File::open(input_file)?;
-    let f = BufReader::new(f);
+impl Parser {
+    // Opens file/stream, make every other component ready to handle it
+    pub fn new(input_file : String) -> Self {
+        let f = File::open(input_file).unwrap();
+        let b = BufReader::new(f);
 
-    for line in f.lines() {
-        println!("{}", line.unwrap());
+        // for line in b.by_ref().lines() {
+        //     println!("{}", line.unwrap());
+        // }
+
+        Parser { target_descriptor: b, current_command: (Command::C_CALL, "".to_string()), next_command: (Command::C_CALL, "".to_string())}
     }
 
-    Ok(())
-}
+    // Are there more command in the input?
+    // (bool, bool) - first one IsEndOfInput?, second one is CorrectCommand
+    pub fn has_more_commands(&mut self) -> (bool, bool) {
+        for line in self.target_descriptor.by_ref().lines() {
+            if let Ok(str) = line {
+                if str.starts_with("push") {
+                    self.next_command = (Command::C_CALL, str);
+                    return (false, true)
+                }
+                return (false, false)
+            } else {
+                // ignore this case
+            }
+        }
+        (true, false)
+    }
 
-// Are there more command in the input?
-fn has_more_commands(input : String) -> String {
-    "Hello".to_string()
-}
+    // Reads next command from the input and makes it the current command
+    // Should be called only if has_more_commmand is true. Initially - no current_command
+    pub fn advance(&mut self) -> bool {
+        ::std::mem::swap(&mut self.current_command, &mut self.next_command);
+        // self.current_command = self.next_command.take();
+        true
+    }
 
-// Reads next command from the input and makes it the current command
-// Should be called only if has_more_commmand is true. Initially - no current_command
-fn advance(command : Command) -> bool {
-    true
-}
-
-// Returns enum variant representing the current_command. I might not need it, because I can pattern match :)
-fn command_type(raw_line : String) -> Command {
-  Command::C_ARITHMETIC
+    // Returns enum variant representing the current_command. I might not need it, because I can pattern match :)
+    pub fn command_type(&self) -> Command {
+        Command::C_ARITHMETIC
+    }
 }
