846a67273e1b000e9e6bbdcc18381e814dd4d221
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-05-14 13:16:58 +0300

    add Ackerman function, it might work really slow

--- haskell_experiments/will_kurt/8.2.3_implementing_cycle.hs
+++ haskell_experiments/will_kurt/8.2.3_implementing_cycle.hs
@@ -5,3 +5,9 @@ finiteCycle (x:xs) = x:xs ++ [x]
 
 myCycle :: [a] -> [a]
 myCycle (x:xs) = x:myCycle (xs++[x])
+
+
+-- Ackerman function
+ackerman 0 n = n + 1
+ackerman m 0 = ackerman (m - 1) 1
+ackerman m n = ackerman (m - 1) (ackerman m (n - 1))
