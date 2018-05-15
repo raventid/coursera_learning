5006ef66546395b4a5a13f714076cdbaa27a736c
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-05-15 23:19:31 +0300

    add collatz function (was not able to hang in)

--- haskell_experiments/will_kurt/8.2.3_implementing_cycle.hs
+++ haskell_experiments/will_kurt/8.2.3_implementing_cycle.hs
@@ -11,3 +11,10 @@ myCycle (x:xs) = x:myCycle (xs++[x])
 ackerman 0 n = n + 1
 ackerman m 0 = ackerman (m - 1) 1
 ackerman m n = ackerman (m - 1) (ackerman m (n - 1))
+
+
+-- Collatz function
+collatz 1 = 1
+collatz n = if even n
+            then 1 + collatz (n `div` 2)
+            else 1 + collatz (n*3 + 1)
