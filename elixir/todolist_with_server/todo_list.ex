49b40fd24cf281da6a6f8753c50aa46f907ad108
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-06-11 21:08:25 +0300

    just a bit more practive with todo_list and gen_server

---
+++ elixir/todolist_with_server/todo_list.ex
@@ -0,0 +1,39 @@
+defmodule TodoServer do
+  def start do
+    spawn(fn -> loop(TodoList.new()) end)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    send(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    send(todo_server, {:entries, self(), date})
+
+    receive do
+      {:todo_entries, entries} -> entries
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
+
+  defp loop(todo_list) do
+    new_todo_list =
+      receive do
+        message -> process_message(todo_list, message)
+      end
+
+    loop(new_todo_list)
+  end
+
+  defp process_message(todo_list, {:add_entry, new_entry}) do
+    TodoList.add_entry(todo_list, new_entry)
+  end
+
+  defp process_message(todo_list, {:entries}) do
+    TodoList.entries(todo_list)
+  end
+end
+
+defmodule TodoList do
+end
