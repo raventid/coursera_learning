1fe4c64c83b832493d21848933c8a03409accb2c
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-06-30 00:02:46 +0300

    little note about what's going on

--- haskell_experiments/will_kurt/q6.1_repeat.hs
+++ haskell_experiments/will_kurt/q6.1_repeat.hs
@@ -2,4 +2,7 @@ module JustSomeStuff where
   repeat :: a -> [a]
   repeat v = cycle [v]
 
+  -- If you'd call this somewhere it will hang
+  -- because Haskell will follow reverse implementation
+  -- and he will never reach the end of list to reverse it
   backwordInfinity = reverse [1..]
