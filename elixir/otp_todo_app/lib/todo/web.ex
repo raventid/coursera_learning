4c2b61195def81c21d571b5709589d5335b5adc6
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-07-02 22:03:31 +0300

    some wip configs for todo otp application

--- elixir/otp_todo_app/lib/todo/web.ex
+++ elixir/otp_todo_app/lib/todo/web.ex
@@ -7,7 +7,7 @@ defmodule Todo.Web do
   def child_spec(_arg) do
     Plug.Adapters.Cowboy.child_spec(
       scheme: :http,
-      options: [port: 5454],
+      options: [port: Application.fetch_env!(:todo, :http_port)],
       plug: __MODULE__
     )
   end
