d2d5bd139253bf03d276017c6ab731abb992ac39
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-07-04 22:07:11 +0300

    fixes terrible mistake in GCD

--- haskell_experiments/will_kurt/7.1_myGCD.hs
+++ haskell_experiments/will_kurt/7.1_myGCD.hs
@@ -4,5 +4,9 @@ module GCD where
 
 myGCD a b =  if remainder == 0
              then b
-             else myGCD a remainder
+             else myGCD b remainder
   where remainder = a `mod` b
+
+-- myGCD a b 0 = b
+-- myGCD a b remainder = myGCDa 
+-- myGCD a b remainder = 
