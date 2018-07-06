ff1ba903e28d8dea38f042d0417d4a95dff6d37b
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-07-07 00:03:12 +0300

    use custom rcons without flip for custom reverse

--- haskell_experiments/will_kurt/9.4_my_reverse.hs
+++ haskell_experiments/will_kurt/9.4_my_reverse.hs
@@ -1,4 +1,13 @@
 module MyReverse where
 
+rcons :: [a] -> a -> [a]
+rcons x y = y:x
+
+myExampleReverse :: [a] -> [a]
+myExampleReverse xs = foldl rcons [] xs
+
+
+-- Looks much better with `flip`
+-- my personal preference...
 myReverse :: String -> String
 myReverse str = foldl (flip (:)) [] str
