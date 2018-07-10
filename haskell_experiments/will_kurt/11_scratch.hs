d1df7af5bd357844f745f269b92d9ecb68025b0e
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-07-10 11:30:43 +0300

    scratch buffer for chapter 11

---
+++ haskell_experiments/will_kurt/11_scratch.hs
@@ -0,0 +1,8 @@
+module Chapter11 where
+
+-- myAverage list = sum list / length list
+
+
+-- 11.2
+printDouble :: Int -> String
+printDouble n = show $ fromIntegral n / 1 -- Maybe there is a better way?
