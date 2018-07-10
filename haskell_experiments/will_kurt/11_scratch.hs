aa408dd1868bf269b5391e423e39e77bdc7c841e
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-07-10 11:32:48 +0300

    note about my failed task

--- haskell_experiments/will_kurt/11_scratch.hs
+++ haskell_experiments/will_kurt/11_scratch.hs
@@ -3,6 +3,6 @@ module Chapter11 where
 -- myAverage list = sum list / length list
 
 
--- 11.2
+-- 11.2 lol, didn't read the task and made it more complicated, ahah
 printDouble :: Int -> String
 printDouble n = show $ fromIntegral n / 1 -- Maybe there is a better way?
