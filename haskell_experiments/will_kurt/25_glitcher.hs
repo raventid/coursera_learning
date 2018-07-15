e9ac845b9adba0390538aedd31e81f66348d00b9
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-07-15 14:23:12 +0300

    first version of glitcher with randomSortSection and replaceByte

---
+++ haskell_experiments/will_kurt/25_glitcher.hs
@@ -0,0 +1,50 @@
+import System.Environment
+import System.Random
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+
+intToChar :: Int -> Char
+intToChar int = toEnum safeInt
+  where safeInt = int `mod` 255
+
+intToBC :: Int -> BC.ByteString
+intToBC int = BC.pack [intToChar int]
+
+replaceByte :: Int -> Int -> BC.ByteString -> BC.ByteString
+replaceByte loc charVal bytes = mconcat [before, newChar, after]
+  where (before, rest) = BC.splitAt loc bytes
+        after = BC.drop 1 rest
+        newChar = intToBC charVal
+
+randomReplaceByte :: BC.ByteString -> IO BC.ByteString
+randomReplaceByte bytes = do
+  let bytesLength = BC.length bytes
+  location <- randomRIO (1, bytesLength)
+  charVal <- randomRIO (0, 255)
+  return (replaceByte location charVal bytes)
+
+sortSection :: Int -> Int -> BC.ByteString -> BC.ByteString
+sortSection start size bytes = mconcat [before, changed, after]
+  where (before, rest) = BC.splitAt start bytes
+        (target, after) = BC.splitAt size rest
+        changed = BC.reverse (BC.sort target)
+
+randomSortSection :: BC.ByteString -> IO BC.ByteString
+randomSortSection bytes = do
+  let sectionSize = 25
+  let bytesLength = BC.length bytes
+  start <- randomRIO (0, bytesLength - sectionSize)
+  return (sortSection start sectionSize bytes)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let fileName = head args
+
+  imageFile <- BC.readFile fileName
+  glitched <- randomSortSection imageFile
+
+  let glitchedFileName = mconcat ["glitched_", fileName]
+  BC.writeFile glitchedFileName glitched
+
+  print "all done"
