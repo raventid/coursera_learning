303ef4b5dacece18d2b5b51a7eb1dcc61e5cb7b2
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-07-17 16:27:25 +0300

    a couple of exercises for applicative

--- haskell_experiments/will_kurt/29_applicative_continue_worksheet.hs
+++ haskell_experiments/will_kurt/29_applicative_continue_worksheet.hs
@@ -6,3 +6,16 @@ boxMultiplier = [10]
 
 calculate :: [Integer]
 calculate = pure (*) <*> doorPrize <*> boxMultiplier
+
+allFmap :: Applicative f => (a -> b) -> f a -> f b
+allFmap f v =  fmap f v
+
+
+spec1 = allFmap (+ 1) [1,2,3] == [2,3,4]
+spec2 = allFmap (+ 1) (Just 5) == Just 6
+spec3 = allFmap (+ 1) Nothing == Nothing
+
+specs = all (\x -> x == True) [spec1, spec2, spec3]
+
+example :: Maybe Int
+example = (pure (*)) <*> (pure ((+) 2 4)) <*> pure 6
