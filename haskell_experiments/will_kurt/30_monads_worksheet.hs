3979fcc4bfae24bb5657bc7714b41898391aa1da
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-07-17 17:47:35 +0300

    trying to crack join with functor or applicative

---
+++ haskell_experiments/will_kurt/30_monads_worksheet.hs
@@ -0,0 +1,31 @@
+import qualified Data.Map as Map
+
+type UserName = String
+type GamerId = Int
+type PlayerCredits = Int
+
+userNameDB :: Map.Map GamerId UserName
+userNameDB = Map.fromList [(1,"nYarlathoTep")
+                          ,(2,"KINGinYELLOW")
+                          ,(3,"dagon1997")
+                          ,(4,"rcarter1919")
+                          ,(5,"xCTHULHUx")
+                          ,(6,"yogSOThoth")]
+
+creditsDB :: Map.Map UserName PlayerCredits
+creditsDB = Map.fromList [("nYarlathoTep",2000)
+                         ,("KINGinYELLOW",15000)
+                         ,("dagon1997",300)
+                         ,("rcarter1919",12)
+                         ,("xCTHULHUx",50000)
+                         ,("yogSOThoth",150000)]
+
+lookupUserName :: GamerId -> Maybe UserName
+lookupUserName id = Map.lookup id userNameDB
+
+lookupCredits :: UserName -> Maybe PlayerCredits
+lookupCredits username = Map.lookup username creditsDB
+
+creditsFromId :: GamerId -> Maybe PlayerCredits -- It's join, actually.
+creditsFromId id = lookupCredits <$> maybeId
+  where maybeId = lookupUserName id
