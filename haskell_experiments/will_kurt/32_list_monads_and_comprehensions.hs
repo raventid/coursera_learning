16e0acc496763b913ec3b38fd7e8934ef19b0eef
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-07-18 17:32:50 +0300

    filter function with do-notation

--- haskell_experiments/will_kurt/32_list_monads_and_comprehensions.hs
+++ haskell_experiments/will_kurt/32_list_monads_and_comprehensions.hs
@@ -5,3 +5,13 @@ evensGuard n = do
   val <- [1..n]
   guard (even val)
   return val
+
+-- Filter function using do-notation.
+-- Looks cool! Btw.
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter f xs = do
+  val <- xs
+  guard $ f val
+  return val
+
+-- TODO: implement my own Alternative typeclass and guard function.
