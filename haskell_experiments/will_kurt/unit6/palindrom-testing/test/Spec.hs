f4a11e8589557f85ad80e3a0042887d41677e19d
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-07-19 20:18:37 +0300

    add unit test, one of them is failing which is OK

--- haskell_experiments/will_kurt/unit6/palindrom-testing/test/Spec.hs
+++ haskell_experiments/will_kurt/unit6/palindrom-testing/test/Spec.hs
@@ -1,2 +1,15 @@
+import Lib
+
+assert :: Bool -> String -> String -> IO ()
+assert test passStatement failStatement = if test
+                                          then putStrLn passStatement
+                                          else putStrLn failStatement
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  putStrLn "Running tests..."
+  assert (isPalindrom "racecar") "passed 'racecar'" "FAIL: 'racecar'"
+  assert (isPalindrom "racecar!") "passed 'racecar!'" "FAIL: 'racecar!'"
+  assert ((not . isPalindrom) "cat") "passed 'cat'" "FAIL: 'cat'"
+  assert (isPalindrom "racecar.") "passed 'racecar.'" "FAIL: 'racecar.'"
+  putStrLn "All tests finished."
