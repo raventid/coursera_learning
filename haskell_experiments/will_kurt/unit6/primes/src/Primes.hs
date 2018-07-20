35ad96813954eabdf3421acd28598ddefbaf982e
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-07-20 17:12:19 +0300

    not finished stack based prime number generation app

---
+++ haskell_experiments/will_kurt/unit6/primes/src/Primes.hs
@@ -0,0 +1,9 @@
+module Primes where
+
+sieve :: [Int] -> [Int]
+sieve [] = []
+sieve (nextPrime:rest) = nextPrime : sieve noFactors
+  where noFactors = filter (not . (== 0) . (`mod` nextPrime)) rest
+
+primes :: [Int]
+primes = [1 .. ]
