7b1ca2e58662a50806c1be5fa04015a2c11960e3
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-07-22 01:46:52 +0300

    bubble sort with stateful array in Haskell

--- haskell_experiments/will_kurt/unit7/42_uarray.hs
+++ haskell_experiments/will_kurt/unit7/42_uarray.hs
@@ -43,3 +43,22 @@ swapST (x,y) = runST $ do
    xfinal <- readSTRef x'
    yfinal <- readSTRef y'
    return (xfinal,yfinal)
+
+-- Highest form of Haskell
+-- Buble sort on stateful array
+myData :: UArray Int Int
+myData = listArray (0,5) [7,6,4,8,10,2]
+
+bubbleSort :: UArray Int Int -> UArray Int Int
+bubbleSort myArray = runSTUArray $ do
+  stArray <- thaw myArray
+  let end = (snd . bounds) myArray
+  forM_ [1 .. end] $ \i -> do
+    forM_ [0 .. (end-1)] $ \j -> do
+      val <- readArray stArray j
+      nextVal <- readArray stArray (j+1)
+      let outOfOrder = val > nextVal
+      when outOfOrder $ do
+        writeArray stArray j nextVal
+        writeArray stArray (j+1) val
+  return stArray
