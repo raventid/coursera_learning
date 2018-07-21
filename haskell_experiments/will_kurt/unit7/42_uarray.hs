c1b4b786517aeb42979486f1b37f14213076faaf
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-07-22 01:24:00 +0300

    Statefull unboxed array from UArray

--- haskell_experiments/will_kurt/unit7/42_uarray.hs
+++ haskell_experiments/will_kurt/unit7/42_uarray.hs
@@ -1,4 +1,7 @@
 import Data.Array.Unboxed
+import Data.Array.ST
+import Control.Monad
+import Control.Monad.ST
 
 zeroIndexArray :: UArray Int Bool
 zeroIndexArray = array (0,9) [(3,True)]
@@ -17,3 +20,15 @@ addTwoToEachElement = accum (+) updateBiB $ zip [0 .. 3] $ cycle [2]
 
 doubleEachElement :: UArray Int Int
 doubleEachElement = accum (*) addTwoToEachElement $ zip [0 .. 3] $ cycle [2]
+
+listToSTUArray :: [Int] -> ST s (STUArray s Int Int)
+listToSTUArray vals = do
+  let end = length vals - 1
+  stArray <- newArray (0, end) 0
+  forM_ [0 .. end] $ \i -> do
+    let val = vals !! i
+    writeArray stArray i val
+  return stArray
+
+listToUArray :: [Int] -> UArray Int Int
+listToUArray vals = runSTUArray $ listToSTUArray vals
