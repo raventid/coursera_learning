5155c14b1bc169b5fa8cae1c9987ecb86cba9d96
Julian Kulesh <juliankul@gmail.com>
2018-08-02 00:18:43 +0300

    wrap log in refcell

--- rust/programming_rust_notes/spider_robot.rs
+++ rust/programming_rust_notes/spider_robot.rs
@@ -4,11 +4,14 @@ use std::cell::Cell;
 // I think I can think of Cell as getter/setter pair
 // You can not get the ref value from Cell. It gives you copy of value
 // you mutate this value somewhere in your program and set it back with setter.
+
+// BTW File is not Copyable, so Cell, want help us. (we need mut File to do something with it)
 pub struct SpiderRobot {
     species: String,
     web_enabled: bool,
     leg_devices: [fd::FileDesc, 8],
     hardware_error_count: Cell<u32>,
+    log: RefCell<File>// with refcell everything is better
 }
 
 impl SpiderRobot {
@@ -20,6 +23,11 @@ impl SpiderRobot {
 
     pub fn has_hardware_errors(&self) {
         self.hardware_error_count.get() > 0
+    }
+
+    pub fn log(&self, message: &str) {
+        let mut file = self.log_file.borrow_mut();
+        writeln!(file, "{}", message).unwrap();
     }
 }
 
