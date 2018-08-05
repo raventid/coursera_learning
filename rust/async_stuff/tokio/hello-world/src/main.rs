dfb3884aec7858beffc95ec4955744a7171ddb52
Julian Kulesh <juliankul@gmail.com>
2018-08-05 11:15:04 +0300

    a little play with tokio

---
+++ rust/async_stuff/tokio/hello-world/src/main.rs
@@ -0,0 +1,23 @@
+extern crate tokio;
+
+use tokio::io;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+
+fn main() {
+    let addr = "127.0.0.1:6142".parse().unwrap();
+    let listener = TcpListener::bind(&addr).unwrap();
+
+    let server = listener.incoming().for_each(|socket| {
+        println!("accepted socket; addr={:?}", socket.peer_addr().unwrap());
+
+        // Process socket here.
+
+        Ok(())
+    }).map_err(|err| {
+        // All tasks must have an `Error` type of `()`
+        // This forces error handling and helps avoid silent failures.
+        // In our example, we are only going to log the error to STDOUT.
+        println!("accept error = {:?}", err)
+    })
+}
