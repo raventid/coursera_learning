4c182a8461f17df3321e97b8a6ee779c65e3cdfd
Julian Kulesh <juliankul@gmail.com>
2018-08-06 20:41:55 +0300

    add example Task and Executor

--- rust/async_stuff/tokio/hello-world/src/main.rs
+++ rust/async_stuff/tokio/hello-world/src/main.rs
@@ -11,7 +11,12 @@ fn main() {
     let server = listener.incoming().for_each(|socket| {
         println!("accepted socket; addr={:?}", socket.peer_addr().unwrap());
 
-        // Process socket here.
+        let connection = io::write_all(socket, "hello world\n").then(|res| {
+            println!("wrote message; success={:?}", res.is_ok());
+        });
+
+        // spawn new task to process the socket
+        tokio::spawn(connection);
 
         Ok(())
     }).map_err(|err| {
@@ -23,4 +28,52 @@ fn main() {
 
     println!("server running on localhost:6142");
     tokio::run(server);
+}
+
+
+// A task that polls a single widget and writes it to STDOUT
+pub struct MyTask;
+
+impl Future for MyTask {
+    // The value this future will have when ready
+    type Item = ();
+    type Error = ();
+
+    fn poll(&mut self) -> Result<Async<()>, ()> {
+        match poll_widget() {
+            Async::Ready(widget) => {
+                println!("widget={:?}", widget);
+                Ok(Async::Ready(()))
+            }
+            Async::NotReady => {
+                Ok(Async::NotReady)
+            }
+        }
+    }
+}
+
+pub struct SpinExecutor {
+    // the tasks an executor is responsible for in a double queue
+    tasks: VecDeque<Box<Future<Item=(), Error=()>>>,
+}
+
+impl SpinExecutor {
+    pub fn spawn<T>(&mut self, task: T)
+        where T: Future<Item=(), Error=()> + 'static
+    {
+        self.tasks.push_back(Box::new(task))
+    }
+
+    pub fn run(&mut self) {
+        // Of course, this would not be very efficient.
+        // The executor spins in a busy loop and tries to poll all tasks even if the task will just return NotReady again.
+        while let Some(mut task) = self.tasks.pop_front() {
+            match task.poll().unwrap() {
+                Async::Ready(_) => {}
+                Async::NotReady => {
+                    self.tasks.push_back(task);
+                }
+            }
+        }
+    }
 }
