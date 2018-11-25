e13344f4a1c0f1881d59705bf22a4552314841b1
Julian Pokrovsky <juliankul@gmail.com>
2018-11-25 20:08:27 +0300

    exercise nandb

---
+++ logical_foundation/basics/nandb.v
@@ -0,0 +1,14 @@
+Definition nandb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => negb b2
+  | false => true
+  end.
+
+Example test_nandb1: (nandb true false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb2: (nandb false false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb3: (nandb false true) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb4: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.
