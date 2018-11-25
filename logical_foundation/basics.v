b609b6dc7893d2a83ecd49f9d45e739f89165dc7
Julian Pokrovsky <juliankul@gmail.com>
2018-11-25 18:51:34 +0300

    basic script for logical foundation

---
+++ logical_foundation/basics.v
@@ -0,0 +1,23 @@
+Inductive day : Type :=
+  | monday : day
+  | tuesday : day
+  | wednesday : day
+  | thursday : day
+  | friday : day
+  | saturday : day
+  | sunday : day.
+
+Definition next_weekday (d:day) : day :=
+ match d with
+ | monday => tuesday
+ | tuesday => wednesday
+ | wednesday => thursday
+ | thursday => friday
+ | friday => monday
+ | saturday => monday
+ | sunday => monday
+ end.
+
+Example test_next_weekday: (next_weekday (next_weekday saturday)) = tuesday.
+Proof. simpl. reflexivity. Qed.
+
