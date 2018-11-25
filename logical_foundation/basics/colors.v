c3ea51b3cceb5d7241a53d808b76f1250e61de42
Julian Pokrovsky <juliankul@gmail.com>
2018-11-26 00:15:23 +0300

    add colors (example of compound type)

---
+++ logical_foundation/basics/colors.v
@@ -0,0 +1,24 @@
+Inductive rgb : Type := 
+  | red : rgb
+  | green : rgb
+  | blue : rgb.
+
+Inductive color : Type :=
+  | black : color
+  | white : color
+  | primary : rgb -> color.
+
+Definition monochrome (c : color) : bool :=
+  match c with
+  | black => false
+  | white => true
+  | primary p => false
+  end.
+
+Definition isred (c : color) : bool :=
+  match c with
+  | black => false
+  | white => false
+  | primary red => true
+  | primary _ => false
+  end.
