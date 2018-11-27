294f1ae549bed3f5707db0201e7d60fdf37ad8a2
Julian Pokrovsky <juliankul@gmail.com>
2018-11-27 18:25:41 +0300

    blt_nat ready made

--- logical_foundation/basics/e_blt_nat.v
+++ logical_foundation/basics/e_blt_nat.v
@@ -1,3 +1,21 @@
+(* Test if numbers are equal *)
+Fixpoint beq_nat (n m : nat) : bool :=
+  match n with
+  | O => match m with
+        | O => true
+        | S m' => false
+        end
+  | S n' => match m with
+           | O => false
+           | S m' => beq_nat n' m'
+           end
+  end.
+Example test_beq_nat1: (beq_nat 2 2) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_beq_nat2: (beq_nat 2 3) = false.
+Proof. simpl. reflexivity. Qed.
+
+(* Test if first number is smaller or equal to second*)
 Fixpoint leb (n m : nat) : bool :=
   match n with
   | O => true
@@ -14,9 +32,12 @@ Proof. simpl. reflexivity. Qed.
 Example test_leb3: (leb 4 2) = false.
 Proof. simpl. reflexivity. Qed.
 
-
-Definition blt_nat (n m : nat) : bool := leb m n.
-Proof. simpl. reflexivity. Qed.
+(* Synthetic function with a complicated meaning :-) *)
+Definition blt_nat (n m : nat) : bool :=
+  match beq_nat n m with
+  | true => false
+  | false => leb n m
+  end.
 Example test_blt_nat1: (blt_nat 2 2) = false.
 Proof. simpl. reflexivity. Qed.
 Example test_blt_nat2: (blt_nat 2 4) = true.
