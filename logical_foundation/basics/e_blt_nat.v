11d69f634b13fa6cd75877a8757f5209d752ead2
Julian Pokrovsky <juliankul@gmail.com>
2018-11-27 18:10:32 +0300

    bugfixes for blt_nat

--- logical_foundation/basics/e_blt_nat.v
+++ logical_foundation/basics/e_blt_nat.v
@@ -7,15 +7,19 @@ Fixpoint leb (n m : nat) : bool :=
     | S m' => leb n' m'
     end
   end.
-
 Example test_leb1: (leb 2 2) =  true.
-Example test_leb2: (leb 2 4) = false.
-Example test_leb3: (leb 4 2) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_leb2: (leb 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_leb3: (leb 4 2) = false.
+Proof. simpl. reflexivity. Qed.
 
 
-Definition blt_nat (n m : nat) : bool := leb n m.
-
+Definition blt_nat (n m : nat) : bool := leb m n.
+Proof. simpl. reflexivity. Qed.
 Example test_blt_nat1: (blt_nat 2 2) = false.
+Proof. simpl. reflexivity. Qed.
 Example test_blt_nat2: (blt_nat 2 4) = true.
+Proof. simpl. reflexivity. Qed.
 Example test_blt_nat3: (blt_nat 4 2) = false.
-Example test_blt_nat4: (blt_nat 0 10) = false.
+Proof. simpl. reflexivity. Qed.
