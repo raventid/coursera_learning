3abe2a812bc8eed6ca42e0a9fec4ecb3d2fe0950
Julian Pokrovsky <juliankul@gmail.com>
2018-11-30 23:11:51 +0300

    updates to proof by simplification

--- logical_foundation/basics/proof_by_simplification.v
+++ logical_foundation/basics/proof_by_simplification.v
@@ -1,3 +1,12 @@
 Theorem plus_O_n : forall n : nat, 0 + n = n.
 Proof.
   intros n. reflexivity. Qed.
+
+(* why not plus_1_n? *)
+Theorem plus_1_1 : forall n:nat, 1 + n = S n.
+Proof.
+  intros n. reflexivity. Qed.
+
+Theorem mult_0_1 : forall n:nat, 0 * n = 0.
+Proof.
+  intros n. reflexivity. Qed.
