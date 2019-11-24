e3b9d34b375a164ef13c292a47629b1cba1feaf6
Julian Pokrovsky <juliankul@gmail.com>
2019-11-25 00:00:09 +0300

    add character parser to hjp

--- haskell/json_parser_by_hand.hs
+++ haskell/json_parser_by_hand.hs
@@ -9,5 +9,17 @@ data JsonValue =
   | JObject [(String, JsonValue)] -- No Map in standard library ;)
   deriving (Show, Eq)
 
+newtype Parser a = Parser
+  { runParser :: String -> Maybe (String, a) }
+
+characterParser :: Char -> Parser Char
+characterParser c = Parser $ \input ->
+  case input of
+    y : ys | y == c -> Just (ys, y)
+    [] -> Nothing
+
+jNull :: Parser JsonValue
+jNull = undefined
+
 main :: IO ()
 main = putStrLn "Placeholder"
