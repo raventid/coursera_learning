14213616565e277ac7ae8b82d7735813468a36c3
Julian Pokrovsky <juliankul@gmail.com>
2019-11-25 11:39:01 +0300

    little note to future me about json parser

--- haskell/json_parser_by_hand.hs
+++ haskell/json_parser_by_hand.hs
@@ -1,5 +1,10 @@
 module JsonParser where
 
+-- We are going to use hand-made parser-combinators.
+-- We are going to cover full JSON standard.
+-- In the end comparison with another libraries and implementations
+-- Maybe we'll be able to imrove speed by only using a better datastructure from containers package
+
 data JsonValue =
     JNull
   | JBool Bool
