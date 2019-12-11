ed8926b5cf6eccf56eb3d32c7a406ad4ce61ce87
Julian Pokrovsky <juliankul@gmail.com>
2019-12-12 00:21:13 +0300

    add needed extensions and imports to project

--- optics/project/Main.hs
+++ optics/project/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Control.Lens
+import Control.Applicative
+import Data.Char
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.Text as T
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
