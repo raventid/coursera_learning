47727219af54eb7eec940638a5a84bdf871914a0
Julian Pokrovsky <juliankul@gmail.com>
2019-12-13 01:03:37 +0300

    add custom and automatic Lens'

--- optics/project/Main.hs
+++ optics/project/Main.hs
@@ -1,5 +1,3 @@
-module Main where
-
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -8,12 +6,53 @@ module Main where
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 
+module Main where
+
 import Control.Lens
-import Control.Applicative
-import Data.Char
-import qualified Data.Map as M
-import qualified Data.Set as S
-import qualified Data.Text as T
+import Control.Lens.TH
+-- import Control.Applicative
+-- import Data.Char
+-- import qualified Data.Map as M
+-- import qualified Data.Set as S
+-- import qualified Data.Text as T
+
+-- data Ship = Ship {
+--   _name :: String
+-- , _numCrew :: Int
+-- } deriving (Show)
+
+-- getNumCrew :: Ship -> Int
+-- getNumCrew = _numCrew
+
+-- setNumCrew :: Ship -> Int -> Ship
+-- setNumCrew ship newNumCrew = ship{_numCrew = newNumCrew}
+
+-- numCrew :: Lens' Ship Int
+-- numCrew = lens getNumCrew setNumCrew
+
+-- getName :: Ship -> String
+-- getName = _name
+
+-- setName :: Ship -> String -> Ship
+-- setName ship newName = ship{_name = newName}
+
+-- name :: Lens' Ship String
+-- name = lens getName setName
+
+
+data Wand = Wand
+data Book = Book
+data Potion = Potion
+
+data Inventory =
+  Inventory {
+    _wand :: Wand
+  , _book :: Book
+  , _potions :: [Potion]
+  }
+
+makeLenses ''Inventory
+
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn "Stub main"
