31d6237c165c968d705d2af391a0dab4950b9e11
Julian Pokrovsky <juliankul@gmail.com>
2019-12-14 23:53:09 +0300

    note about lens law

--- optics/project/Main.hs
+++ optics/project/Main.hs
@@ -9,6 +9,7 @@
 module Main where
 
 import Control.Lens
+import qualified Control.Lens.Unsound as U
 import Control.Applicative
 import Data.Char
 import qualified Data.Map as M
@@ -54,6 +55,38 @@ makeLenses ''Inventory
 
 -- Now we can use our TH lenses with any combinator
 -- view wand $ Inventory Wand Book [Potion]
+
+
+-- Lens which fails first law (set-get)
+type UserName = String
+type UserId = String
+
+data Session =
+  Session { _userId :: UserId
+          , _userName :: UserName
+          , _createdTime :: String
+          , _expiryTime :: String
+          } deriving (Show, Eq)
+
+makeLenses ''Session
+
+userInfo :: Lens' Session (UserId, UserName)
+userInfo = U.lensProduct userId userName
+
+-- session = Session "USER-1234" "Joey Tribbiani" "2019-07-25" "2019-08-25"
+-- view userInfo session
+
+alongsideUserId :: Lens' Session (Session, UserId)
+alongsideUserId = U.lensProduct id userId
+
+-- session = Session "USER-1234" "Joey Tribbiani" "2019-07-25" "2019-08-25"
+-- newSession = session{_userId="USER-5678"}
+-- view alongsideUserId (set alongsideUserId (newSession, "USER-9999") session)
+-- view alongsideUserId (set alongsideUserId (newSession, "USER-9999") session)
+
+-- The problem here is that:
+-- view alongsideUserId (set alongsideUserId (newSession, "USER-9999") session) == (newSession, "USER-9999")
+-- I cannot completely agree with this (now), we do not get what we set in session field, but it is not a session lens. It is alongsideUseId lens, so why should it return "USER-9999" if it is not a session lens? Maybe from a formal point of view name of lens does not matter. You set some focus, and if you read this focus with the same lens you should see it (Makes sense!)
 
 main :: IO ()
 main = putStrLn "Stub main"
