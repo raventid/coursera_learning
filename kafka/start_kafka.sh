4043bf654a1987d6e0e830ce26e69d2d3bc81e9e
Julian Pokrovsky <juliankul@gmail.com>
2020-02-08 01:43:18 +0300

    some kafka starting scripts

---
+++ kafka/start_kafka.sh
@@ -0,0 +1,5 @@
+bin/zookeeper-server-start.sh config/zookeeper.properties
+
+bin/kafka-server-start.sh config/server0.properties
+bin/kafka-server-start.sh config/server1.properties
+bin/kafka-server-start.sh config/server2.properties
