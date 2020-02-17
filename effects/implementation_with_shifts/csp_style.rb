da3beb686c8b5c2595396b014d22bd2a08851455
Julian Pokrovsky <juliankul@gmail.com>
2020-02-17 15:47:15 +0300

    add cps interpreter

--- effects/implementation_with_shifts/csp_style.rb
+++ effects/implementation_with_shifts/csp_style.rb
@@ -2,20 +2,40 @@
 
 def evalulate(exp, env, next_)
   if exp.is_a? Numeric
-    exp
+    puts "Numeric: #{exp}, env: #{env}"
+    next_.(exp)
   elsif exp.is_a? String
-    env[exp]
+    puts "String: #{exp}, env: #{env}"
+    next_.(env[exp])
   elsif exp[:type] == "add"
-    evalulate(exp[:exp1], env, ?) + evalulate(exp[:exp2], env, ?)
+    puts "add: #{exp}, env: #{env}"
+    evalulate(exp[:exp1], env, ->(v1) {
+        puts "In function: #{exp}, env: #{env}"
+        evalulate(exp[:exp2], env, ->(v2) {
+            puts "Before addition: #{exp}, env: #{env}"
+            next_.(v1 + v2)
+          })
+      })
   elsif exp[:type] == "fun"
-    -> (value) do
+    puts "fun: #{exp}, env: #{env}"
+    f = -> (value, next_) do
+      puts "building Lambda env: #{exp}, env: #{env}"
       fun_env = env.merge({ exp[:param] => value })
-      evalulate(exp[:body], fun_env, ?)
+      puts "evaling lambda: #{exp}, env: #{env}"
+      evalulate(exp[:body], fun_env, next_)
     end
+
+    puts "fun: #{exp}, env: #{env}"
+    next_.(f)
   elsif exp[:type] == "call"
-    fun_value = evalulate(exp[:fun_exp], env, ?) # to use CPS I should use callbacks here!
-    arg_value = evalulate(exp[:arg_exp], env, ?) # I need a diagram with explanation
-    fun_value.(arg_value)
+    puts "call: #{exp}, env: #{env}"
+      evalulate(exp[:fun_exp], env, ->(fun_value) {
+          puts "call_second: #{exp}, env: #{env}"
+          evalulate(exp[:arg_exp], env, ->(arg_value) {
+              puts "call_final: #{exp}, env: #{env}"
+              fun_value.(arg_value, next_)
+            })
+      })
   end
 end
 
@@ -35,4 +55,4 @@ double_fun = fun("x", add("x", "x"))
 
 program = call(double_fun, 10)
 
-puts (evalulate program, {})
+puts (evalulate program, {}, ->(x) { x })
