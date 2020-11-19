7555654a76aaf01b21868ca91d42bba757e2c52c
Julian Pokrovsky <juliankul@gmail.com>
2020-11-19 22:26:13 +0300

    make zio duration more visible

--- scala/zio/hello_world/src/main/scala/Main.scala
+++ scala/zio/hello_world/src/main/scala/Main.scala
@@ -4,7 +4,7 @@ import zio.duration._
 
 object Main extends App {
   val runZio = ZIO.effect(println("Hello, World!"))
-  val runZioLater = runZio.delay(10.seconds)
+  val runZioLater = runZio.delay(100.seconds)
 
   def run(args: List[String]) = {
     runZioLater.exitCode
