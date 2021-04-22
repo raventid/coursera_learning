2089cde6e6584cc4e5058afb53d067f4cadc57c2
Julian Pokrovsky <juliankul@gmail.com>
2021-04-22 11:27:06 +0300

    [algo] implement quck find

---
+++ algo/week1/QuickFindUF.java
@@ -0,0 +1,29 @@
+package week1;
+
+public class QuickFindUF {
+    private int[] id;
+    private int count;
+
+    public QuickFindUF(int n) {
+        count = n;
+        id = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+    }
+
+    public boolean connected(int p, int q) {
+        return id[p] == id[q];
+    }
+
+    public void union(int p, int q) {
+        int pid = id[p];
+        int qid = id[q];
+
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pid) {
+                id[i] = qid;
+            }
+        }
+    }
+}
