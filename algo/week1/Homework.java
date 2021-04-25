27910918f618942ce5c971c44b540c4e68c10519
Julian Pokrovsky <juliankul@gmail.com>
2021-04-25 22:06:15 +0300

    [algo] starting percolation

---
+++ algo/week1/Homework.java
@@ -0,0 +1,10 @@
+package week1;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Homework {
+   public static void main(String[] args) {
+   }
+}
