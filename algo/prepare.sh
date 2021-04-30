0913fe8a377231a6b3d277e9a7f01ce6cba976e9
Julian Pokrovsky <juliankul@gmail.com>
2021-04-30 11:05:10 +0300

    [algo] update script to create zip files

--- algo/prepare.sh
+++ algo/prepare.sh
@@ -3,3 +3,5 @@ set -euo pipefail
 
 sed -i '' '/package /d' ./week1/percolation/Percolation.java
 sed -i '' '/package /d' ./week1/percolation/PercolationStats.java
+
+zip percolation.zip ./week1/percolation/Percolation.java ./week1/percolation/PercolationStats.java
