5c45a09f1bef3230656b12ec13d5afe3ef9609bc
Julian Pokrovsky <juliankul@gmail.com>
2021-05-01 21:41:43 +0300

    [algo] update prepare script

--- algo/prepare.sh
+++ algo/prepare.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sed -i '' '/package /d' ./week1/percolation/Percolation.java
-sed -i '' '/package /d' ./week1/percolation/PercolationStats.java
+cp ./week1/percolation/Percolation.java ./Percolation.java
+cp ./week1/percolation/PercolationStats.java ./PercolationStats.java
 
-zip percolation.zip ./week1/percolation/Percolation.java ./week1/percolation/PercolationStats.java
+sed -i '' '/package /d' ./Percolation.java
+sed -i '' '/package /d' ./PercolationStats.java
+
+zip percolation.zip ./Percolation.java ./PercolationStats.java
