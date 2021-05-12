0a94499e4c01b6fefcc0b2ddaf4f0e1fa41224d4
Julian Pokrovsky <juliankul@gmail.com>
2021-05-12 22:29:39 +0300

    [algo] refactore PercolateStats

--- algo/week1/percolation/PercolationStats.java
+++ algo/week1/percolation/PercolationStats.java
@@ -2,96 +2,74 @@ package week1.percolation;
 
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import java.lang.Math;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class PercolationStats {
-    // n-by-n grid
-    private int n;
-
-    // # of trials
-    private int trials;
-
-    // results of percolations
     private double[] results;
 
-    // the average value in results
-    private double mean;
+    public PercolationStats(int size, int trials) {
+        assertPositive(size);
+        assertPositive(trials);
 
-    // sample standard deviation of percolation threshold
-    private double stddev;
+        results = new double[trials];
+        runExperiments(size, trials);
+    }
 
-    // low endpoint of 95% confidence interval
-    private double cfdLow;
-
-    // high endpoint of 95% confidence interval
-    private double cfdHigh;
-
-    // confidence level at 95%
-    private final static double CFD_CRITICAL_VAL = 1.96;
-
-    // perform trials independent experiments on an n-by-n grid
-    public PercolationStats(int n, int trials) {
-        if (n <= 0 || trials <= 0) {
-            throw new java.lang.IllegalArgumentException("arguments n and trials should be greater than 0");
+    private void assertPositive(int num) {
+        if (num < 1) {
+            throw new IllegalArgumentException();
         }
+    }
 
-        this.n = n;
-        this.trials = trials;
-        this.results = new double[trials];
+    private void runExperiments(int size, int trials) {
+        for (int i = 0; i < trials; i++) {
+            results[i] = runExperiment(size);
+        }
+    }
 
-        for(int i = 0; i < trials; i++) {
-            Percolation sy = new Percolation(n);
-            while(!sy.percolates()) {
-                int row = StdRandom.uniform(1, n+1);
-                int col = StdRandom.uniform(1, n+1);
-                sy.open(row, col);
+    private double runExperiment(int size) {
+        Percolation p = new Percolation(size);
+        int openCells = 0;
+        do {
+            int row = random(size);
+            int col = random(size);
+            if (!p.isOpen(row, col)){
+                p.open(row, col);
+                openCells++;
             }
+        } while (!p.percolates());
 
-            double result = (double)sy.numberOfOpenSites() / (n * n);
-            this.results[i] = result;
-        }
-
-        this.mean = StdStats.mean(results);
-        this.stddev = StdStats.stddev(results);
-        this.cfdLow = this.mean - PercolationStats.CFD_CRITICAL_VAL * (this.stddev / Math.sqrt(trials));
-        this.cfdHigh = this.mean + PercolationStats.CFD_CRITICAL_VAL * (this.stddev / Math.sqrt(trials));
+        return (double) openCells/((double) size*size);
     }
 
-    // sample mean of percolation threshold
+    private int random(int size) {
+        return StdRandom.uniform(1, size + 1);
+    }
+
     public double mean() {
-        return this.mean;
+        return StdStats.mean(results);
     }
 
-    // sample standard deviation of percolation threshold
     public double stddev() {
-        return this.stddev;
+        return StdStats.stddev(results);
     }
 
-    // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return this.cfdLow;
+        return this.mean() - this.confidence();
     }
 
-    // high endpoint of 95% confidence interval
+    private double confidence() {
+        return (1.96 * stddev() / Math.sqrt(results.length));
+    }
+
     public double confidenceHi() {
-        return this.cfdHigh;
+        return this.mean() + this.confidence();
     }
 
-    // test client (described below)
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        int T = Integer.parseInt(args[1]);
-
-        System.out.println(String.format("%-20s= %d", "n", n));
-        System.out.println(String.format("%-20s= %d", "T", T));
-
-        try {
-            PercolationStats statsOne = new PercolationStats(n, T);
-            System.out.println(String.format("%-20s= %f" , "mean", statsOne.mean()));
-            System.out.println(String.format("%-20s= %f" , "stddev", statsOne.stddev()));
-            System.out.println(String.format("%-20s= %f, %f" , "95% confidence interval", statsOne.confidenceLo(), statsOne.confidenceHi()));
-        } catch(IllegalArgumentException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+        PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        System.out.println("mean                    = " + stats.mean());
+        System.out.println("stddev                  = " + stats.stddev());
+        System.out.println("95% confidence interval = " + stats.confidenceLo() + ", " + stats.confidenceHi());
     }
 }
