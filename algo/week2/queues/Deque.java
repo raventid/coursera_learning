fe00efb86d8f642e8f35291ca827d15e5abfc7c8
Julian Pokrovsky <juliankul@gmail.com>
2021-05-14 22:10:11 +0300

    [algo] starting deque.java

--- algo/week2/queues/Deque.java
+++ algo/week2/queues/Deque.java
@@ -1,28 +1,61 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class Deque<Item> implements Iterable<Item> {
 
+    private Node leftGuard, rightGuard;
+    private int size = 0;
+
+    private class Node {
+        Item item;
+        Node next;
+        Node prev;
+    }
+
     // construct an empty deque
-    public Deque()
+    public Deque() {
+        leftGuard = new Node();
+        rightGuard = new Node();
+
+        // we are using guards as a markers, they have null in item.
+        leftGuard.next = rightGuard;
+        rightGuard.prev = leftGuard;
+    }
 
     // is the deque empty?
-    public boolean isEmpty()
+    public boolean isEmpty() {
+        leftGuard.next == rightGuard
+    }
 
     // return the number of items on the deque
-    public int size()
+    public int size() {
+        return size;
+    }
 
     // add the item to the front
-    public void addFirst(Item item)
+    public void addFirst(Item item) {
+        size += 1;
+    }
 
     // add the item to the back
-    public void addLast(Item item)
+    public void addLast(Item item) {
+        size += 1;
+    }
 
     // remove and return the item from the front
-    public Item removeFirst()
+    public Item removeFirst() {
+        size -= 1;
+        return null;
+    }
 
     // remove and return the item from the back
-    public Item removeLast()
+    public Item removeLast() {
+        size -= 1;
+        return null;
+    }
 
     // return an iterator over items in order from front to back
-    public Iterator<Item> iterator()
+    // public Iterator<Item> iterator() {}
 
     // unit testing (required)
     public static void main(String[] args)
