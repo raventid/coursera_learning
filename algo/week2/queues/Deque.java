898e5fb478c645ba2d7935df473a5b42cf27163f
Julian Pokrovsky <juliankul@gmail.com>
2021-05-14 07:59:12 +0300

    [algo] starting week2

---
+++ algo/week2/queues/Deque.java
@@ -0,0 +1,30 @@
+public class Deque<Item> implements Iterable<Item> {
+
+    // construct an empty deque
+    public Deque()
+
+    // is the deque empty?
+    public boolean isEmpty()
+
+    // return the number of items on the deque
+    public int size()
+
+    // add the item to the front
+    public void addFirst(Item item)
+
+    // add the item to the back
+    public void addLast(Item item)
+
+    // remove and return the item from the front
+    public Item removeFirst()
+
+    // remove and return the item from the back
+    public Item removeLast()
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator()
+
+    // unit testing (required)
+    public static void main(String[] args)
+
+}
