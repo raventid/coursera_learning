3ff25054c972e01fd8c2c825bf8cdd0dba89beed
Julian Pokrovsky <juliankul@gmail.com>
2021-05-26 21:22:02 +0300

    [algo] starting random queue

---
+++ algo/week2/queues/RandomizedQueue.java
@@ -0,0 +1,63 @@
+package week2.queues;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    private int[] storage;
+
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+        this.storage = new int[10];
+    }
+
+    // is the randomized queue empty?
+    public boolean isEmpty() {
+        return true;
+    }
+
+    // return the number of items on the randomized queue
+    public int size() {
+        return 0;
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+    }
+
+    // remove and return a random item
+    public Item dequeue() {
+        return null;
+    }
+
+    // return a random item (but do not remove it)
+    public Item sample() {
+        return null;
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+        return new Iter();
+    }
+
+    private class Iter implements Iterator<Item> {
+        public boolean hasNext() {
+            return false;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            return null;
+        }
+    }
+
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+
+}
