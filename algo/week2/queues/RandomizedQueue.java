95f8770e972df703aa6acb0aed9e1ab9c52b2436
Julian Pokrovsky <juliankul@gmail.com>
2021-05-30 21:12:05 +0300

    [algo] wip rq 1

--- algo/week2/queues/RandomizedQueue.java
+++ algo/week2/queues/RandomizedQueue.java
@@ -5,24 +5,31 @@ import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private Item[] storage;
+    private int nextSlot;
+    private int size;
 
     // construct an empty randomized queue
     public RandomizedQueue() {
         this.storage = (Item[]) new Object[10];
+        this.nextSlot = 0;
+        this.size = 0;
     }
 
     // is the randomized queue empty?
     public boolean isEmpty() {
-        return true;
+        return size == 0;
     }
 
     // return the number of items on the randomized queue
     public int size() {
-        return 0;
+        return size;
     }
 
     // add the item
     public void enqueue(Item item) {
+        this.storage[this.nextSlot] = item;
+        this.nextSlot += 1;
+        this.size +=1;
     }
 
     // remove and return a random item
@@ -56,8 +63,10 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // unit testing (required)
     public static void main(String[] args) {
-        var randQeueu = new RandomizedQueue<Integer>();
+        var rq = new RandomizedQueue<Integer>();
 
-
+        rq.enqueue(10);
+        rq.enqueue(20);
+        rq.enqueue(30);
     }
 }
