8f9241258b79c53b2dbc9eb1478e655273f5baa8
Julian Pokrovsky <juliankul@gmail.com>
2021-06-08 21:02:11 +0300

    [algo] permutation client

---
+++ algo/week2/queues/Permutation.java
@@ -0,0 +1,12 @@
+package week2.queues;
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class Permutation {
+   public static void main(String[] args) {
+       while (!StdIn.isEmpty()) {
+           String input = StdIn.readString();
+           System.out.println(input);
+       }
+   }
+}
