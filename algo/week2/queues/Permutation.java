2e6914b0d2cf9f1bde6b29ba8149d490918234a1
Julian Pokrovsky <juliankul@gmail.com>
2021-06-10 21:03:12 +0300

    [algo] week2 permutation is done

--- algo/week2/queues/Permutation.java
+++ algo/week2/queues/Permutation.java
@@ -4,8 +4,14 @@ import edu.princeton.cs.algs4.StdIn;
 
 public class Permutation {
    public static void main(String[] args) {
+       var rq = new RandomizedQueue<String>();
+
        while (!StdIn.isEmpty()) {
            String input = StdIn.readString();
+           rq.enqueue(input);
+       }
+
+       for (String input : rq) {
            System.out.println(input);
        }
    }
