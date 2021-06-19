4b66d4ea2b7d66e5067ff73d119863edd3a42feb
Julian Pokrovsky <juliankul@gmail.com>
2021-06-19 22:37:12 +0300

    [katas] update solution for one of the problems

--- katas/leetcode/1089.Duplicate_Zeros.rs
+++ katas/leetcode/1089.Duplicate_Zeros.rs
@@ -16,3 +16,35 @@ impl Solution {
         *arr = tmp;
     }
 }
+
+
+
+
+// Inplace version
+
+impl Solution {
+    pub fn duplicate_zeros(arr: &mut Vec<i32>) {
+        let length = arr.len();
+        let mut zeroes = 0;
+
+        for i in arr.iter() {
+            if *i == 0 { zeroes += 1 }
+        }
+
+        for i in (0..length).rev() {
+            Self::shift(arr, length, i, zeroes);
+            if (arr[i] == 0) { zeroes -= 1 }
+        }
+    }
+
+    fn shift(arr: &mut Vec<i32>, length: usize, index: usize, shift: usize) {
+        if (index + shift < length) {
+            arr[index+shift] = arr[index];
+        }
+
+        // we should not only copy zero to shifted position, but also duplicate it on -1 position
+        if (index - 1 + shift < length && arr[index] == 0) {
+            arr[index+shift-1] = arr[index]
+        }
+    }
+}
