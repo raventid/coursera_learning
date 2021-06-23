113848da8bc0b6a6534af787a58d3976a305534e
Julian Pokrovsky <juliankul@gmail.com>
2021-06-23 21:12:58 +0300

    [algo] Binet implementation with more precision

--- katas/other/fibonacci.rs
+++ katas/other/fibonacci.rs
@@ -1,15 +1,15 @@
-// Fibbonacci with Binet's formula
-// bad take, it does not work
+// Fibbonacci with Binet's formula. Works only for small numbers. Breaks around 100.
+// https://stackoverflow.com/questions/9645193/calculating-fibonacci-number-accurately-in-c
 fn main() {
     (0..100).for_each(|number: u32| {
-        let value = binet(number);
+        let value = binet(number as f64);
 
         println!("For {} Value is {}", number, value);
     })
 }
 
-fn binet(number: u32) -> i32 {
-    let sr_5 = (5 as f32).sqrt() + 1.;
-    let result = (1. + sr_5 / 2.).powf(number as f32) / sr_5;
-    result as i32
+fn binet(number: f64) -> u64 {
+    let phi : f64 = (1_f64 + 5_f64.sqrt()) * 0.5;
+    let fib = (phi.powf(number) - (1_f64 - phi).powf(number)) / 5_f64.sqrt();
+    fib as u64
 }
