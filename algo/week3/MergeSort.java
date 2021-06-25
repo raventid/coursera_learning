e9a287c7362a0e9c7b6ac51156c4b4fb6df9e331
Julian Pokrovsky <juliankul@gmail.com>
2021-06-25 22:05:11 +0300

    [algo] final mergesort impelemntation

--- algo/week3/MergeSort.java
+++ algo/week3/MergeSort.java
@@ -1,11 +1,25 @@
 package week3;
 
 class MergeSort {
+    private static Comparable[] aux;
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) { return; }
+        int mid = lo + (hi - lo)/2;
+        sort(a, lo, mid);
+        sort(a, mid+1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
     private static void merge(Comparable[] a, int lo, int mid, int hi) {
         int i = lo, j = mid+1;
 
         for (int k = lo; k <= hi; k++) {
-            aux[k] = a[k]
+            aux[k] = a[k];
         }
 
         for (int k = lo; k <= hi; k++) {
@@ -16,7 +30,22 @@ class MergeSort {
         }
     }
 
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        var vector =  new String[]{ "dog", "cat", "parrot", "julian", "raventid" };
+        for (var e : vector) {
+            System.out.println(e);
+        }
+        sort(vector);
+        System.out.println("--------------------");
+        System.out.println("After sorting");
+        System.out.println("--------------------");
+        for (var e : vector) {
+            System.out.println(e);
+        }
     }
 }
