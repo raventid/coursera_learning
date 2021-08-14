71796f22842c371c6f4265b105499f2a9864d523
Julian Pokrovsky <juliankul@gmail.com>
2021-08-14 20:30:06 +0300

    [katas] additional option for running_sum

--- katas/leetcode/1480.Running_Sum_of_1d_Array.rs
+++ katas/leetcode/1480.Running_Sum_of_1d_Array.rs
@@ -3,3 +3,14 @@ impl Solution {
         nums.iter().fold((0, vec![]), |(sum, mut vec), i| (sum + i, { vec.push(sum + i); vec }) ).1
     }
 }
+
+impl Solution {
+    pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+        nums.into_iter().fold(Vec::new(), |mut running_sum, elem| {
+            match running_sum.last() {
+                Some(last) => { running_sum.push(last + elem); running_sum },
+                None => { running_sum.push(elem); running_sum }
+            }
+        })
+    }
+}
