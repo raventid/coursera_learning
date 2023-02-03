5eaf3121c79b304e6929c95cd7a59020d97143c7
Julian Pokrovsky <juliankul@gmail.com>
2023-02-03 16:13:57 +0800

    rust spinlock inits

---
+++ rust/sync/spinlock.rs
@@ -0,0 +1,19 @@
+pub struct SpinLock {
+    locked: AtomicBool,
+}
+
+impl SpinLock {
+    pub const fn new() -> Self {
+        Self { locked: AtomicBool::new(false) }
+    }
+
+    pub fn lock(&self) {
+        while self.locked.swap(true, Acquire) {
+            std::hint::spin_loop();
+        }
+    }
+
+    pub fn unlock(&self) {
+        self.locked.store(false, Release);
+    }
+}
