8f0cd93f49ed894c99eb82a5b59cfb34367b69eb
Julian Pokrovsky <juliankul@gmail.com>
2023-02-05 14:51:09 +0800

    rust spinlock safe impl with guard

--- rust/sync/spinlock.rs
+++ rust/sync/spinlock.rs
@@ -29,3 +29,72 @@ impl<T> SpinLock<T> {
         self.locked.store(false, Release);
     }
 }
+
+// Version with a guard
+
+pub struct Guard<'a, T> {
+    lock: &'a SpinLock<T>
+}
+
+impl<T> SpinLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    // returning Guard with lock inside
+    pub fn lock(&self) -> Guard<T> {
+        while self.locked.swap(true, Acquire) {
+            std::hint::spin_loop();
+        }
+        Guard { lock: self }
+    }
+
+    pub unsafe fn unlock(&self) {
+        self.locked.store(false, Release);
+    }
+}
+
+use std::ops::{Deref, DerefMut};
+
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        // Safety: The very existence of this Guard
+        // guarantees we've exclusively locked the lock.
+        unsafe { &*self.lock.value.get() }
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        // Safety: The very existence of this Guard
+        // guarantees we've exclusively locked the lock.
+        unsafe { &mut *self.lock.value.get() }
+    }
+}
+
+impl<T> Drop for Guard<'_, T> {
+    // Remove guard and unlock the value
+    fn drop(&mut self) {
+        self.lock.locked.store(false, Release);
+    }
+}
+
+// Usage of a second version
+fn main() {
+    let x = SpinLock::new(Vec::new());
+    thread::scope(|s| {
+        s.spawn(|| x.lock().push(1));
+        s.spawn(|| {
+            let mut g = x.lock();
+            g.push(2);
+            g.push(2);
+        });
+    });
+    let g = x.lock();
+    assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
+}
