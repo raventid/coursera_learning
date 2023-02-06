3c420a3571ccaf892daebea9c5dbf54ed26e8f82
Julian Pokrovsky <juliankul@gmail.com>
2023-02-06 16:01:15 +0800

    rust mutex base channel

---
+++ rust/sync/mutex_based_channel.rs
@@ -0,0 +1,28 @@
+pub struct Channel<T> {
+    queue: Mutex<VecDeque<T>>,
+    item_ready: CondVar
+}
+
+impl<T> Channel<T> {
+    pub fn new() -> Self {
+        Self {
+            queue: Mutex::new(VecDeque::new()),
+            item_ready: Condvar::new(),
+        }
+    }
+
+    pub fn send(&self, message: T) {
+        self.queue.lock().unwrap().push_back(message);
+        self.item_ready.notify_one();
+    }
+
+    pub fn receive(&self) -> T {
+        let mut b = self.queue.lock().unwrap();
+        loop {
+            if let Some(message) = b.pop_front() {
+                return message;
+            }
+            b = self.item_ready.wait(b).unwrap();
+        }
+    }
+}
