dd3c334e4a674c59d272fb5f00f3311350c9bc19
Julian Pokrovsky <juliankul@gmail.com>
2023-02-07 16:24:55 +0800

    rust one shot channel init

---
+++ rust/sync/one_shot_channel_with_atomic.rs
@@ -0,0 +1,17 @@
+use std::mem::MaybeUninit;
+
+pub struct Channel<T> {
+    message: UnsafeCell<MaybeUninit<T>>,
+    ready: AtomicBool,
+}
+
+unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+impl<T> Channel<T> {
+    pub const fn new() -> Self {
+        Self {
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            ready: AtomicBool::new(false),
+        }
+    }
+}
