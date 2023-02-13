148d2d4c0d9e9f94cd8daaae5bb1105de60aa884
Julian Pokrovsky <juliankul@gmail.com>
2023-02-13 18:43:46 +0800

    rust locks continue

--- rust/sync/locks/src/main.rs
+++ rust/sync/locks/src/main.rs
@@ -1,3 +1,12 @@
+use std::{sync::atomic::{self, AtomicU32}, cell::UnsafeCell};
+
+pub struct Mutex<T> {
+    // 0: unlocked
+    // 1: locked
+    state: AtomicU32,
+    value: UnsafeCell<T>
+}
+
 fn main() {
     println!("Hello, world!");
 }
