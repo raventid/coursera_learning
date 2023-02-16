1daac7a1a4930d57218f0815cb0c39bc71a850b7
Julian Pokrovsky <juliankul@gmail.com>
2023-02-16 11:25:00 +0800

    start cses katas

---
+++ katas/cses/weird_algorithm.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  while (true) {
+    cout << n << " ";
+
+    if (n == 1) break;
+    if (n % 2 == 0) n /= 2;
+    else n = n * 3 + 1;
+  }
+  cout << "\n";
+}
