54a8902d42aede1e182266018ee07a3dabc67f2e
Julian Pokrovsky <juliankul@gmail.com>
2023-02-23 17:39:17 +0800

    kafka: remove fully

--- kafka/start_kafka.sh
+++
@@ -1,5 +0,0 @@
-bin/zookeeper-server-start.sh config/zookeeper.properties
-
-bin/kafka-server-start.sh config/server0.properties
-bin/kafka-server-start.sh config/server1.properties
-bin/kafka-server-start.sh config/server2.properties
