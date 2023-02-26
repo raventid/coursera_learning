2d8e117f096aab35feae4fa7e409cdc5be6e001c
Julian Pokrovsky <juliankul@gmail.com>
2023-02-27 01:56:50 +0800

    katas: working solution, but not fast

--- katas/cses/missing_number.cpp
+++ katas/cses/missing_number.cpp
@@ -1,17 +1,30 @@
 // g++ -ggdb -O0 -o missing_number missing_number.cpp && ./missing_number
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
   int n;
+  std::vector<int> input;
 
   cin >> n;
-  for (int i = 1; i <= n; i++) {
+
+  // read input to vec (n-1 because 1 number is missing)
+  for (int i = 0; i < n-1; i++) {
     int m;
     cin >> m;
-    if (i != m) {
+    input.push_back(m);
+  }
+
+  // sort vec O(n*logn)
+  sort(input.begin(), input.end());
+
+  // output the element
+  for (int i = 1; i <= n; i++) {
+    if (i != input[i-1]) {
       cout << i;
       break;
     }
